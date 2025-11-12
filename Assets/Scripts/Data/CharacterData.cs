24473127fc515e6c6377cc764dd45ff72e00ed81
NaritaShosei <onigirikun7171@gmail.com>
2025-11-12 18:46:01 +0900

    [add]データクラスの作成

---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameData/CharacterData", fileName = "CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    [Header("キャラクターの名前")]
+    [SerializeField] private string _name;
+    [Header("キャラクターのパラメータ")]
+    [SerializeField] private float _maxHP;
+    [SerializeField] private float _moveSpeed;
+    public string Name => _name;
+    public float MaxHP => _maxHP;
+    public float MoveSpeed => _moveSpeed;
+}
