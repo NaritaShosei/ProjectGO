0f24533a07db2d7517f91b960aeccfe65dd68a6e
NaritaShosei <onigirikun7171@gmail.com>
2025-11-16 17:10:49 +0900

    [fix]AttackDataを修正

--- Assets/Scripts/Data/AttackData.cs
+++ Assets/Scripts/Data/AttackData.cs
@@ -5,16 +5,17 @@ public class AttackData : ScriptableObject
 {
     [Header("基本情報")]
     [SerializeField] private string _attackName;
-    [SerializeField] private float _power;
-    [SerializeField] private float _range;
+    [SerializeField] private float _power = 1;
+    [SerializeField, Tooltip("攻撃の範囲(直径)")] private float _range = 1;
     [SerializeField] private float _speed;
-    [SerializeField, Tooltip("ノックバックの強さ")] private float _knockbackForce;
-    [SerializeField, Tooltip("ノックバックの向き")] private Vector3 _knockbackDirection;
+    [SerializeField, Tooltip("ノックバックの強さ")] private float _knockbackForce = 1;
+    [SerializeField, Tooltip("ノックバックの向き")] private Vector3 _knockbackDirection = Vector3.up;
 
     [Header("モーション設定")]
     [SerializeField] private AnimationClip _animationClip;
     [SerializeField] private AudioClip _soundEffect;
-    [SerializeField] private float _motionDuration;
+    [Header("アニメーションがないとき用の仮のモーション時間")]
+    [SerializeField] private float _motionDuration = 1;
 
     [Header("次の攻撃（コンボ遷移）")]
     [SerializeField] private AttackData _nextCombo;
@@ -35,7 +36,7 @@ public class AttackData : ScriptableObject
 
     public AnimationClip AnimationClip => _animationClip;
     public AudioClip SoundEffect => _soundEffect;
-    public float MotionDuration => _motionDuration;
+    public float MotionDuration => _animationClip ? _animationClip.length : _motionDuration;
 
     public AttackData NextCombo => _nextCombo;
 }
