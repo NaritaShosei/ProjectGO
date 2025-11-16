c96663b329838e6709d0343872aaa39f095c1d76
NaritaShosei <onigirikun7171@gmail.com>
2025-11-16 17:12:57 +0900

    [add]攻撃を依頼するクラスの作成

---
+++ Assets/Scripts/Player/AttackHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class AttackHandler : MonoBehaviour
+{
+    [Header("攻撃の判定位置")]
+    [SerializeField] private Transform _attackPoint;
+
+    private AttackData _currentData;
+
+    public void SetupData(AttackData data)
+    {
+        _currentData = data;
+    }
+
+    /// <summary>
+    /// AnimationEventで呼び出す想定のメソッド
+    /// </summary>
+    public void ApplyAttack()
+    {
+        var data = _currentData;
+
+        // 攻撃範囲からIEnemyを継承したオブジェクトを取得し攻撃する
+        var colls = Physics.OverlapSphere(_attackPoint.position, data.Range / 2);
+
+        foreach (var coll in colls)
+        {
+            if (!coll.TryGetComponent(out IEnemy enemy)) { continue; }
+
+            enemy.AddDamage(data.Power);
+
+            // ノックバックの方向・強さを計算
+            var knockbackForce = data.KnockbackForce * data.KnockbackDirection;
+
+            enemy.AddKnockBackForce(knockbackForce);
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying || !_currentData) { return; }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_attackPoint.position, _currentData.Range / 2);
+
+        // 攻撃方向の表示
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(_attackPoint.position, _attackPoint.forward * _currentData.Range / 2);
+    }
+#endif
+}
