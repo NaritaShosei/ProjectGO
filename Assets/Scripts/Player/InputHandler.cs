6e09e6bbef176f080b4ad54368969217a6a8b5b6
NaritaShosei <onigirikun7171@gmail.com>
2025-11-18 15:32:27 +0900

    [update]入力管理クラスに各行動のイベントを追加

--- Assets/Scripts/Player/InputHandler.cs
+++ Assets/Scripts/Player/InputHandler.cs
@@ -6,9 +6,13 @@ public class InputHandler : MonoBehaviour
     private PlayerInput _input;
 
     public Vector2 MoveInput { get; private set; }
+
+    public event Action OnDodge;
     public event Action OnLightAttack;
     public event Action OnChargeStart;
     public event Action OnChargeEnd;
+    public event Action OnHeal;
+    public event Action OnModeChange;
 
     private void OnEnable()
     {
@@ -17,10 +21,16 @@ public class InputHandler : MonoBehaviour
         _input.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
         _input.Player.Move.canceled += _ => MoveInput = Vector2.zero;
 
+        _input.Player.Dodge.started += _ => OnDodge?.Invoke();
+
         _input.Player.LightAttack.performed += _ => OnLightAttack?.Invoke();
         _input.Player.ChargeAttack.started += _ => OnChargeStart?.Invoke();
 
         _input.Player.ChargeAttack.canceled += _ => OnChargeEnd?.Invoke();
+
+        _input.Player.Heal.started += _ => OnHeal?.Invoke();
+
+        _input.Player.ModeChange.started += _ => OnModeChange?.Invoke();
 
         _input.Enable();
     }
