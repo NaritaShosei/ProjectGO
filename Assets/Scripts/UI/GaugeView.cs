20ea0667c042449f51e09feb9a8cf01bcf4f7618
NaritaShosei <onigirikun7171@gmail.com>
2025-11-22 15:34:46 +0900

    [add]ゲージUIの作成

---
+++ Assets/Scripts/UI/GaugeView.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeView : MonoBehaviour
+{
+    [SerializeField] private Image _gauge;
+    [SerializeField] private Image _backgroundGauge;
+    [SerializeField] private float _duration = 0.5f;
+    [SerializeField] private float _delay = 0.5f;
+    [SerializeField] private Ease _animEase = Ease.Linear;
+    private Sequence _gaugeSeq;
+
+    public void Init(float current, float max)
+    {
+        var amount = current / max;
+
+        _gauge.fillAmount = amount;
+        _backgroundGauge.fillAmount = amount;
+    }
+
+    public void UpdateGauge(float current, float max)
+    {
+        var amount = current / max;
+        _gauge.fillAmount = amount;
+
+        // 再生中のアニメーションを中断
+        _gaugeSeq?.Kill();
+
+        // HPゲージを更新した後少し遅らせて背景のゲージを更新
+        _gaugeSeq = DOTween.Sequence().
+            Append(_backgroundGauge.DOFillAmount(amount, _duration)).
+            SetDelay(_delay).
+            SetEase(_animEase).
+            SetLink(gameObject);
+    }
+}
