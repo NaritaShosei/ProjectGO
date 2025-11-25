d446c940bec9210c700bef524b211a8444320428
NaritaShosei <onigirikun7171@gmail.com>
2025-11-25 22:22:17 +0900

    [add]回復するアイテム

---
+++ Assets/Scripts/Interact/HealItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HealItem : MonoBehaviour, IInteractable
+{
+    [Header("UI表示する説明")]
+    [SerializeField] private string _exception;
+    [Header("回復量")]
+    [SerializeField] private float _healValue = 50;
+
+    public string GetInteractText()
+    {
+        return _exception;
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        if (interactor.TryGetComponent(out IHealable component))
+        {
+            component.Healing(_healValue);
+        }
+    }
+}
