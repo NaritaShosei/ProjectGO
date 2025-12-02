ffbf7c319ab73220678ab6128969bc24b68321a6
NaritaShosei <onigirikun7171@gmail.com>
2025-12-02 17:40:35 +0900

    [add]スロー演出などの速度を管理するマネージャー

---
+++ Assets/Scripts/System/SpeedManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour, ISpeedManager
+{
+    [SerializeField] private Slow _slow;
+
+    public event Action<float> OnSpeedChanged;
+    public Slow Slow => _slow;
+
+    private float _speed = 1;
+
+    private void Awake()
+    {
+        _slow.Init(this);
+    }
+
+    public void UpdateSpeed()
+    {
+        float newSpeed = _speed * _slow.Value;
+        OnSpeedChanged?.Invoke(newSpeed);
+    }
+}
