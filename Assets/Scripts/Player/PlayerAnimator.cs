e03b4a15025471d06260f754b659d10177eaf823
NaritaShosei <onigirikun7171@gmail.com>
2025-12-02 17:51:08 +0900

    [fix]メモリリーク予防

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class PlayerAnimator
+public class PlayerAnimator : IDisposable
 {
     private Animator _animator;
-
+    private ISpeedManager _speedManager;
     public PlayerAnimator(ISpeedManager speedManager, Animator anim)
     {
-        speedManager.OnSpeedChanged += ChangeSpeed;
+        _speedManager = speedManager;
+        _speedManager.OnSpeedChanged += ChangeSpeed;
         _animator = anim;
+    }
+
+    public void Dispose()
+    {
+        if (_speedManager is null) { return; }
+
+        _speedManager.OnSpeedChanged -= ChangeSpeed;
     }
 
     private void ChangeSpeed(float speed) => _animator.speed = speed;
