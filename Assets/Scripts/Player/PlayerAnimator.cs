bb0adba75ea1af255fe9d49a57e9dcf674088ce9
NaritaShosei <onigirikun7171@gmail.com>
2025-12-02 17:41:27 +0900

    [add]Playerのアニメーションの再生速度を変更するクラス

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator
+{
+    private Animator _animator;
+
+    public PlayerAnimator(ISpeedManager speedManager, Animator anim)
+    {
+        speedManager.OnSpeedChanged += ChangeSpeed;
+        _animator = anim;
+    }
+
+    private void ChangeSpeed(float speed) => _animator.speed = speed;
+}
