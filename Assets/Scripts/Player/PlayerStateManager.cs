c830758c6f0037f1ffbfa697449b4c773502cdf1
NaritaShosei <onigirikun7171@gmail.com>
2025-12-26 15:40:01 +0900

    feat:プレイヤーの状態管理クラスの作成

---
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class PlayerStateManager
+{
+    public PlayerState CurrentState { get; private set; } = PlayerState.Idle;
+
+    // 状態変更イベント(必要に応じて)
+    public event Action<PlayerState, PlayerState> OnStateChanged;
+
+    public void ChangeState(PlayerState newState)
+    {
+        if (CurrentState == newState) return;
+
+        PlayerState oldState = CurrentState;
+        CurrentState = newState;
+        OnStateChanged?.Invoke(oldState, newState);
+    }
+
+    public bool CanAttack() => CurrentState is PlayerState.Idle or PlayerState.Moving;
+    public bool CanMove() => CurrentState != PlayerState.Attacking
+                          && CurrentState != PlayerState.Dodge
+                          && CurrentState != PlayerState.Damaged
+                          && CurrentState != PlayerState.Dead;
+    public bool CanDodge() => CurrentState is PlayerState.Idle or PlayerState.Moving;
+}   
+public enum PlayerState
+{
+    Idle,
+    Moving,
+    Attacking,
+    Dodge,
+    Damaged,
+    Dead,
+}
