8943307c25358e909157cb3bb8493d502cb31cc5
NaritaShosei <onigirikun7171@gmail.com>
2025-12-26 15:58:31 +0900

    feat:カメラマネージャーの作成

---
+++ Assets/Scripts/System/CameraManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera MainCamera => _mainCamera;
+
+    private Camera _mainCamera;
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+}
