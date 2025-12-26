a65f098a2a59b1d21eba5da9be779077099f3281
NaritaShosei <onigirikun7171@gmail.com>
2025-12-26 16:56:30 +0900

    feat:移動データの作成

---
+++ Assets/Scripts/Data/MoveData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MoveData", menuName = "GameData/MoveData")]
+public class MoveData : ScriptableObject
+{
+    public float MoveSpeed => _moveSpeed;
+    public float RotateSpeed => _rotateSpeed;
+
+    [SerializeField] private float _moveSpeed = 10;
+    [SerializeField] private float _rotateSpeed = 5;
+}
