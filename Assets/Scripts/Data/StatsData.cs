df2019b9df94754c3ce2b28dca91b87808094bde
NaritaShosei <onigirikun7171@gmail.com>
2025-12-29 05:12:00 +0900

    feat:ステータスデータクラスの作成

---
+++ Assets/Scripts/Data/StatsData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatsData", menuName = "GameData/StatsData")]
+
+public class StatsData : ScriptableObject
+{
+    public float MaxHealth => _maxHealth;
+    public float MaxStamina => _maxStamina;
+
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _maxStamina;
+}
