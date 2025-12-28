91a82e56ce8a3c4f786ac1bd0dd762d07e626d28
NaritaShosei <onigirikun7171@gmail.com>
2025-12-29 06:17:01 +0900

    feat:プレイヤーのモードがかわっても変わらないデータの作成

---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "GameData/PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public StatsData Stats => _stats;
+    public float AttackPower => _attackPower;
+    public float BlockPower => _blockPower;
+    public float CriticalRate => _criticalRate;
+    public float HealAmount => _healAmount;
+    public float StaminaRegenPerSecond => _staminaRegenPerSecond;
+    public float DodgeStaminaCost => _dodgeStaminaCost;
+
+    [SerializeField] private StatsData _stats;
+
+    [SerializeField] private float _attackPower;
+    [SerializeField] private float _blockPower;
+
+    // クリティカル発生率（0.0〜1.0想定）
+    [SerializeField] private float _criticalRate;
+
+    // 回復量（1回あたり）
+    [SerializeField] private float _healAmount;
+
+    // スタミナ自然回復量（1秒あたり）
+    [SerializeField] private float _staminaRegenPerSecond;
+
+    // 回避時のスタミナ消費量
+    [SerializeField] private float _dodgeStaminaCost;
+}
