fd22e3134b13d1700ffe4b152149c139e7a61485
NaritaShosei <onigirikun7171@gmail.com>
2025-12-29 20:21:59 +0900

    feat:データを渡す

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -211,7 +211,7 @@ public class PlayerAttack : MonoBehaviour
         _currentAttackId = attackData.AttackId;
 
         // 攻撃実行
-        _attackExecutor.Execute(attackData, input, _modeController);
+        _attackExecutor.Execute(attackData, input, _modeController.ModeData);
 
         _lastAttackTime = Time.time;
 
