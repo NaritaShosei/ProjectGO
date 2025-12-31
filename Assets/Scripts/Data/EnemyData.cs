740f443e3d8b571c592edcd3ed293ecf47211ee7
NaritaShosei <onigirikun7171@gmail.com>
2025-12-31 17:10:59 +0900

    feat:敵のデータ

---
+++ Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "GameData/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float MaxHP => _maxHP;
+    public float MoveSpeed => _moveSpeed;
+
+    public float AttackRange => _attackRange;
+    public float AttackRadius => _attackRadius;
+    public float AttackCooldown => _attackCooldown;
+    public float AttackDamage => _attackDamage;
+
+    [Header("Status")]
+    [SerializeField] private float _maxHP = 100f;
+
+    [Header("Movement")]
+    [SerializeField] private float _moveSpeed = 3f;
+
+    [Header("Attack")]
+    [SerializeField] private float _attackRange = 1.5f;
+    [SerializeField] private float _attackRadius = 1.0f;
+    [SerializeField] private float _attackCooldown = 1.2f;
+    [SerializeField] private float _attackDamage = 10f;
+}
