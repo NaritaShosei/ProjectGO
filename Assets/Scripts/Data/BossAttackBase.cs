2ae44d84bc2e8407ecd167639875dbb61f43fd05
NaritaShosei <onigirikun7171@gmail.com>
2026-01-10 22:51:14 +0900

    feat:攻撃の基底

---
+++ Assets/Scripts/Data/BossAttackBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossAttackData", menuName = "GameData/BossAttackData")]
+public abstract class BossAttackBase : ScriptableObject
+{
+    public float Interval;
+
+    /// <summary>
+    /// 攻撃実行（各攻撃で中身を書く）
+    /// </summary>
+    public abstract void Execute(BossAttackContext context);
+
+    [SerializeField] protected private float _interval;
+    [SerializeField] protected private float _damage;
+}
+
+public struct BossAttackContext
+{
+    public Transform BossTransform;
+    public Transform Player;
+}
