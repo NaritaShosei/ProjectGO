3c616c237585fbb307ecda16dbc0bd344770aa0f
NaritaShosei <onigirikun7171@gmail.com>
2026-01-10 22:50:10 +0900

    feat:フェーズのデータ

---
+++ Assets/Scripts/Data/BossPhaseData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossPhaseData", menuName = "GameData/BossPhaseData")]
+
+public class BossPhaseData : ScriptableObject
+{
+    public BossAttackBase[] Attacks => _attacks;
+    [SerializeField] private BossAttackBase[] _attacks;
+}
