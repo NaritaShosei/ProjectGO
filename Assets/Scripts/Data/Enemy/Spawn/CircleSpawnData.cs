de6a56c5ab10db5c1ccb2716b54caf7014791c85
NaritaShosei <onigirikun7171@gmail.com>
2026-01-16 20:10:24 +0900

    feat:敵生成のデータ

---
+++ Assets/Scripts/Data/Enemy/Spawn/CircleSpawnData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CircleSpawnData", menuName = "GameData/SpawnData/Circle")]
+
+public class CircleSpawnData : SpawnData
+{
+    public Vector3 Center => _center;
+    public float Radius => _radius;
+
+    public override ISpawnStrategy CreateStrategy(EnemyManager enemyManager)
+    {
+        return new CircleSpawnStrategy(enemyManager);
+    }
+
+    [SerializeField] private Vector3 _center;
+    [SerializeField] private float _radius;
+}
+
+public class CircleSpawnStrategy : ISpawnStrategy
+{
+    private readonly EnemyManager _enemyManager;
+
+    public CircleSpawnStrategy(EnemyManager enemyManager)
+    {
+        _enemyManager = enemyManager;
+    }
+
+    public void Spawn(SpawnData data)
+    {
+        var d = (CircleSpawnData)data;
+
+        for (int i = 0; i < d.Enemies.Length; i++)
+        {
+            float angle = i * Mathf.PI * 2f / d.Enemies.Length;
+            Vector3 pos = d.Center +
+                new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * d.Radius;
+
+            _enemyManager.Spawn(d.Enemies[i], pos);
+        }
+    }
+}
---
+++ Assets/Scripts/Data/Enemy/Spawn/SpawnData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class SpawnData : ScriptableObject
+{
+    public GameObject[] Enemies => _enemies;
+    public abstract ISpawnStrategy CreateStrategy(EnemyManager enemyManager);
+
+    [SerializeField] private GameObject[] _enemies;
+}
