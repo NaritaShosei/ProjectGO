3c856244129289cbe003382052b169384e20349f
Oriku0308 <O.riku8863@gmail.com>
2026-01-16 20:20:04 +0900

    refactor: OnDestroyの部分にnullチェックの追加

--- Assets/Scripts/UI/TitlePanelController.cs
+++ Assets/Scripts/UI/TitlePanelController.cs
@@ -3,7 +3,7 @@
 public class TitlePanelController : MonoBehaviour
 {
     [SerializeField]
-    private TitlePanelView _titlePaneView;
+    private TitlePanelView _titlePanelView;
     private SceneTransitionManager _sceneTransitionManager;
 
     [Header("シーン遷移先")]
@@ -15,16 +15,31 @@ public class TitlePanelController : MonoBehaviour
     private void Start()
     {
         _sceneTransitionManager = ServiceLocator.Get<SceneTransitionManager>();
+
+        if (_sceneTransitionManager == null)
+        {
+            Debug.LogError("SceneTransitionManager is not registered in ServiceLocator.", this);
+            return;
+        }
+        if (_titlePanelView == null)
+        {
+            Debug.LogError("TitlePanelView is not assigned.", this);
+            return;
+        }
+
         // イベントハンドラの登録
-        _titlePaneView.OnModeSelectButton += HandleModeSelectButton;
-        _titlePaneView.OnOptionButton += HandleOptionButton;
+        _titlePanelView.OnModeSelectButton += HandleModeSelectButton;
+        _titlePanelView.OnOptionButton += HandleOptionButton;
     }
 
     private void OnDestroy()
     {
-        // イベントハンドラの登録解除
-        _titlePaneView.OnModeSelectButton -= HandleModeSelectButton;
-        _titlePaneView.OnOptionButton -= HandleOptionButton;
+        if (_titlePanelView != null)
+        {
+            // イベントハンドラの登録解除
+            _titlePanelView.OnModeSelectButton -= HandleModeSelectButton;
+            _titlePanelView.OnOptionButton -= HandleOptionButton;
+        }
     }
 
     private void HandleModeSelectButton()
