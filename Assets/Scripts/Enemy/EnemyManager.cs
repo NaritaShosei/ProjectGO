798cbfc87562e34eeba56f57b1de70ec9af51c89
NaritaShosei <onigirikun7171@gmail.com>
2026-01-17 17:22:59 +0900

    feat:フェーズシステムに合わせた敵管理クラスの調整

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    public event Action OnEnemyDefeated;
+    public event Action OnBossDefeated;
+
     public void Spawn(GameObject original, Vector3 pos)
     {
         var obj = Instantiate(original, pos, Quaternion.identity, parent: transform);
@@ -15,6 +19,29 @@ public class EnemyManager : MonoBehaviour
 
         else { Destroy(obj); Debug.LogWarning("IEnemyを継承していないオブジェクトを生成したため、破壊しました"); }
     }
+    public int GetEnemyCount() => _enemies.Count;
+
+    /// <summary>
+    /// SpawnDataRepositoryから一括生成
+    /// </summary>
+    public void SpawnFromRepository(SpawnDataRepository repository)
+    {
+        if (repository == null || repository.SpawnDatas == null) return;
+
+        foreach (var spawnData in repository.SpawnDatas)
+        {
+            var strategy = spawnData.CreateStrategy(this);
+            strategy.Spawn();
+        }
+    }
+
+    /// <summary>
+    /// ボスを生成
+    /// </summary>
+    public void SpawnBoss(GameObject bossPrefab, Vector3 position)
+    {
+        Spawn(bossPrefab, position);
+    }
 
     private List<IEnemy> _enemies = new();
 
@@ -24,9 +51,18 @@ public class EnemyManager : MonoBehaviour
         {
             enemy.OnDead -= HandleEnemyDead;
             _enemies.Remove(enemy);
+
+            // ボスかどうか判定
+            if (enemy is BossEnemy)
+            {
+                OnBossDefeated?.Invoke();
+            }
+            else
+            {
+                OnEnemyDefeated?.Invoke();
+            }
         }
     }
-
 
     // デバッグ用
     private void OnGUI()
