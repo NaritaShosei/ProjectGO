570de96e9a8d91ab00733b9f25d7f8c90f476b69
NaritaShosei <onigirikun7171@gmail.com>
2026-01-21 18:51:46 +0900

    feat:動作確認用のスキル

---
+++ Assets/Scripts/Data/Skill/TestSkill.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestSkill", menuName = "GameData/Skill/TestSkill")]
+public class TestSkill : SkillBase
+{
+    public override AttackContext Apply(AttackContext context)
+    {
+        context.OnHit += () => GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
+        return context;
+    }
+
+    public override bool CanApply(AttackContext context, AttackData data)
+    {
+        return data.ComboIndex == 2;
+    }
+}
