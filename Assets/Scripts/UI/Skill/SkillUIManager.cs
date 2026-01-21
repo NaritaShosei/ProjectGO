f1ba71b673d4fa7ded6879ea5d8dd57606966ab4
NaritaShosei <onigirikun7171@gmail.com>
2026-01-21 15:11:27 +0900

    feat:skillUIのデバッグ用コード

---
+++ Assets/Scripts/UI/Skill/SkillUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SkillUIManager : MonoBehaviour, ISkillSelectUIManager
+{
+    public event Action OnSkillSelected;
+    public void Show()
+    {
+        _gameObject.SetActive(true);
+    }
+
+    [SerializeField] private GameObject _gameObject;
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            OnSkillSelected?.Invoke();
+            _gameObject.SetActive(false);
+        }
+    }
+}
