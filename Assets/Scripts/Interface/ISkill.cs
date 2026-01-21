99b7652e97288967d2ae1df2acb88a6ad86255ba
NaritaShosei <onigirikun7171@gmail.com>
2026-01-21 18:01:36 +0900

    feat:スキルのインターフェース

---
+++ Assets/Scripts/Interface/ISkill.cs
@@ -0,0 +1,17 @@
+﻿public interface ISkill
+{
+    /// <summary>
+    /// 発動優先度
+    /// </summary>
+    public int Priority { get; }
+
+    /// <summary>
+    /// 発動条件
+    /// </summary>
+    public bool CanApply(AttackContext context);
+
+    /// <summary>
+    /// スキルを発動する
+    /// </summary>
+    public AttackContext Apply(AttackContext context);
+}
