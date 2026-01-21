00bc126c93c2ce8ae9db5d67c8e7909bf4820585
NaritaShosei <onigirikun7171@gmail.com>
2026-01-21 18:49:57 +0900

    feat:スキルの基底クラス

---
+++ Assets/Scripts/Data/Skill/SkillBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SkillBase : ScriptableObject, ISkill
+{
+    public int Priority => _priority;
+
+    public virtual AttackContext Apply(AttackContext context)
+    {
+        return context;
+    }
+
+    public virtual bool CanApply(AttackContext context,AttackData data)
+    {
+        return true;
+    }
+
+    [SerializeField] private int _priority;
+}
