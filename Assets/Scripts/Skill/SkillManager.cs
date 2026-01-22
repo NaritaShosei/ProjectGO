c28a69b970c263d142f860c868863b02d628a23d
NaritaShosei <onigirikun7171@gmail.com>
2026-01-23 00:18:57 +0900

    feat:スキルのマネージャークラスの作成

---
+++ Assets/Scripts/Skill/SkillManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    /// <summary>
+    /// スキルのIDを登録する 登録済みであればfalseが返ってくる
+    /// </summary>
+    public bool TryRegisterSkillId(int id)
+    {
+        if (_ownedSkillIDs.Contains(id))
+        {
+            return false;
+        }
+
+        _ownedSkillIDs.Add(id);
+        return true;
+    }
+
+    /// <summary>
+    /// 開放済みのIDを取得する
+    /// </summary>
+    public List<int> GetOwnedSkillIDs() => _ownedSkillIDs;
+
+    /// <summary>
+    /// スキルの列挙を返す
+    /// </summary>
+    public IEnumerable<SkillBase> GetOwnedSkills()
+    {
+        foreach (var id in _ownedSkillIDs)
+        {
+            var skill = _skillDataBase.GetSkill(id);
+
+            if (skill != null)
+            {
+                yield return skill;
+            }
+        }
+    }
+
+    [SerializeField] private SkillDataBase _skillDataBase;
+
+    private List<int> _ownedSkillIDs = new List<int>();
+}
