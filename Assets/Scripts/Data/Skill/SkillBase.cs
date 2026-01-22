15b29323246cf571d941fe82fff709e5549c4632
NaritaShosei <onigirikun7171@gmail.com>
2026-01-23 00:17:07 +0900

    feat:スキルにIDを追加

--- Assets/Scripts/Data/Skill/SkillBase.cs
+++ Assets/Scripts/Data/Skill/SkillBase.cs
@@ -2,6 +2,7 @@
 
 public class SkillBase : ScriptableObject, ISkill
 {
+    public int ID => _id;
     public int Priority => _priority;
 
     public virtual DamageContext Apply(ref AttackContext context)
@@ -13,10 +14,11 @@ public class SkillBase : ScriptableObject, ISkill
         };
     }
 
-    public virtual bool CanApply(AttackContext context,AttackData data)
+    public virtual bool CanApply(AttackContext context, AttackData data)
     {
         return true;
     }
 
+    [SerializeField] private int _id;
     [SerializeField] private int _priority;
 }
