9932661c8e04489ff7c10cc665f55636e4a97bd8
NaritaShosei <onigirikun7171@gmail.com>
2026-01-23 18:31:50 +0900

    feat:スキル選択UI

---
+++ Assets/Scripts/UI/Skill/SkillSelectButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSelectButton : MonoBehaviour
+{
+    public void Setup(SkillViewData viewData, Action onClick)
+    {
+        _nameText.text = viewData.Name;
+        _explanationText.text = viewData.Explanation;
+        _icon.sprite = viewData.Icon;
+
+        _selectButton.onClick.AddListener(onClick.Invoke);
+    }
+
+    [SerializeField] private Button _selectButton;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _explanationText;
+    [SerializeField] private Image _icon;
+}
