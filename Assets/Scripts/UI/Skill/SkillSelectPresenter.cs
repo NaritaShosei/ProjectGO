6c3bab1a610b1c9f71a0eed48acb577cfeabdabb
NaritaShosei <onigirikun7171@gmail.com>
2026-01-23 18:37:24 +0900

    feat:スキル選択UIのプレゼンター

---
+++ Assets/Scripts/UI/Skill/SkillSelectPresenter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class SkillSelectPresenter
+{
+    public SkillSelectPresenter(
+      SkillManager skillManager,
+      ISkillSelectView view)
+    {
+        _skillManager = skillManager;
+        _view = view;
+
+        _view.OnSkillSelected += OnSkillSelected;
+    }
+
+    /// <summary>
+    /// スキル選択UIを表示する
+    /// </summary>
+    public void Open(int candidateCount)
+    {
+        _currentSkills = _skillManager.GetSelectableSkills(candidateCount);
+
+        var viewData = _currentSkills
+            .Select(s => new SkillViewData(
+                s.ID,
+                s.Name,
+                s.Explanation,
+                null // 必要なら SkillBase に持たせる
+            ))
+            .ToList();
+
+        _view.Show(viewData);
+    }
+
+    private readonly SkillManager _skillManager;
+    private readonly ISkillSelectView _view;
+
+    private List<SkillBase> _currentSkills;
+
+    /// <summary>
+    /// ボタンが押されたときに呼ばれる
+    /// </summary>
+    private void OnSkillSelected(int skillId)
+    {
+        _skillManager.TryRegisterSkillId(skillId);
+        _view.Hide();
+    }
+}
