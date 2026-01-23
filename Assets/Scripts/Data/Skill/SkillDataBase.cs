419f138cbb53cd8d461acf712cc875420a9e5279
NaritaShosei <onigirikun7171@gmail.com>
2026-01-23 20:00:56 +0900

    feat:辞書登録を初回アクセス時にも実行するようにしてnull参照などを起きないように

--- Assets/Scripts/Data/Skill/SkillDataBase.cs
+++ Assets/Scripts/Data/Skill/SkillDataBase.cs
@@ -7,16 +7,29 @@ public class SkillDataBase : ScriptableObject
 
     public SkillBase GetSkill(int skillId)
     {
+        CreateDictionary();
+
         _skillMap.TryGetValue(skillId, out var skill);
         return skill;
     }
 
     public SkillBase[] GetAllSkills() => _skills;
 
+    [SerializeField] private SkillBase[] _skills;
+
     private Dictionary<int, SkillBase> _skillMap;
+
     private void OnEnable()
     {
+        CreateDictionary();
+    }
+
+    private void CreateDictionary()
+    {
+        if (_skillMap != null) { return; }
+
         _skillMap = new Dictionary<int, SkillBase>();
+
         foreach (var skill in _skills)
         {
             if (!skill || _skillMap.ContainsKey(skill.ID))
@@ -28,6 +41,4 @@ public class SkillDataBase : ScriptableObject
             _skillMap[skill.ID] = skill;
         }
     }
-
-    [SerializeField] private SkillBase[] _skills;
 }
