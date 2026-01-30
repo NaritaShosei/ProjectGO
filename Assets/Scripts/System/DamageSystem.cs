72ed9321cf304af743b9b3ed2396517185746d85
NaritaShosei <onigirikun7171@gmail.com>
2026-01-30 17:59:12 +0900

    feat:ダメージ計弾システム

---
+++ Assets/Scripts/System/DamageSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public static class DamageSystem
+{
+    public static int Calculate(
+        DamageContext attack,
+        DefenseContext defense)
+    {
+        float damage =
+            attack.AttackPower
+          * GetEnemyTypeMultiplier(attack.PlayerMode, defense.EnemyType);
+
+        return Mathf.RoundToInt(damage);
+    }
+
+    private static float GetEnemyTypeMultiplier(PlayerMode mode, EnemyType type)
+    {
+        switch (mode)
+        {
+            case PlayerMode.Warrior:
+                switch (type)
+                {
+                    case EnemyType.Armor: return 1.5f;
+                    case EnemyType.Flesh: return 0.8f;
+                }
+                break;
+
+            case PlayerMode.Thunder:
+                switch (type)
+                {
+                    case EnemyType.Armor: return 0.8f;
+                    case EnemyType.Flesh: return 1.5f;
+                }
+                break;
+        }
+
+        return 1.0f; // 保険
+    }
+}
