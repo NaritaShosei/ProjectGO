049c4a222110f332e88ca13d55d1549ff9096be6
NaritaShosei <onigirikun7171@gmail.com>
2026-01-30 19:53:39 +0900

    fix:スキルの戻り値などの変更

--- Assets/Scripts/Data/Skill/TestSkill.cs
+++ Assets/Scripts/Data/Skill/TestSkill.cs
@@ -3,14 +3,9 @@
 [CreateAssetMenu(fileName = "TestSkill", menuName = "GameData/Skill/TestSkill")]
 public class TestSkill : SkillBase
 {
-    public override DamageContext Apply(ref AttackContext context)
+    public override void Apply(ref AttackContext context)
     {
         context.OnHit += () => GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
-        return new DamageContext()
-        {
-            AttackPower = context.AttackPower,
-            PlayerMode = context.PlayerMode,
-        };
     }
 
     public override bool CanApply(AttackContext context, AttackData data)
