49f1bd5d6b4a3970de917ec8ba09100ac0d56f9f
NaritaShosei <onigirikun7171@gmail.com>
2026-02-02 20:16:03 +0900

    feat:アニメーション遷移管理クラス

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public void Init(PlayerStateManager stateManager, IModeController modeController)
+    {
+        _stateManager = stateManager;
+        _modeController = modeController;
+
+        _stateManager.OnStateChanged += OnStateChanged;
+        _modeController.OnModeChanged += OnModeChanged;
+    }
+
+    public void UpdateMoveAnimation(float speed)
+    {
+        _animator.SetFloat(AnimParams.Speed, speed);
+    }
+
+    public void PlayAttack(int attackId)
+    {
+        _animator.SetInteger(AnimParams.AttackId, attackId);
+        _animator.SetTrigger(AnimParams.Attack);
+    }
+
+    public void PlayDodge()
+    {
+        _animator.SetTrigger(AnimParams.Dodge);
+    }
+
+    public void OnDestroy()
+    {
+        if (_stateManager != null)
+            _stateManager.OnStateChanged -= OnStateChanged;
+        if (_modeController != null)
+            _modeController.OnModeChanged -= OnModeChanged;
+    }
+
+    [SerializeField] private Animator _animator;
+
+    // アニメーションパラメータ名（定数化）
+    private static class AnimParams
+    {
+        public static readonly int Speed = Animator.StringToHash("Speed");
+        public static readonly int Attack = Animator.StringToHash("Attack");
+        public static readonly int AttackId = Animator.StringToHash("AttackId");
+        public static readonly int Dodge = Animator.StringToHash("Dodge");
+        public static readonly int IsCharging = Animator.StringToHash("IsCharging");
+        public static readonly int Damaged = Animator.StringToHash("Damaged");
+        public static readonly int Dead = Animator.StringToHash("Dead");
+        public static readonly int PlayerMode = Animator.StringToHash("PlayerMode");
+    }
+
+    private void OnStateChanged(PlayerState oldState, PlayerState newState)
+    {
+        switch (newState)
+        {
+            case PlayerState.Charging:
+                _animator.SetBool(AnimParams.IsCharging, true);
+                break;
+            case PlayerState.Dead:
+                _animator.SetTrigger(AnimParams.Dead);
+                break;
+            case PlayerState.Damaged:
+                _animator.SetTrigger(AnimParams.Damaged);
+                break;
+        }
+
+        if (oldState == PlayerState.Charging)
+        {
+            _animator.SetBool(AnimParams.IsCharging, false);
+        }
+    }
+
+    private void OnModeChanged(PlayerMode newMode)
+    {
+        _animator.SetInteger(AnimParams.PlayerMode, (int)newMode);
+    }
+
+    private PlayerStateManager _stateManager;
+    private IModeController _modeController;
+}
