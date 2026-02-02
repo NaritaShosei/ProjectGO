17b04a61987907019904a745c5c72d50ced0c371
NaritaShosei <onigirikun7171@gmail.com>
2026-02-02 18:37:33 +0900

    fix:移動ロジックの修正

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -57,26 +57,30 @@ public class PlayerMovement : MonoBehaviour
         if (!_playerStateManager.CanMove()) { return; }
 
         var vec = _input.MoveInput;
-        var camera = _cameraManager.MainCamera;
-
         var inputMag = vec.magnitude;
 
-        if (inputMag < INPUT_THRESHOLD)
-        {
-            _rb.linearVelocity = Vector3.zero;
-            return;
-        }
+        if (inputMag < INPUT_THRESHOLD) { return; }
+
+        var camera = _cameraManager.MainCamera;
 
         var right = camera.transform.right * vec.x;
         var forward = camera.transform.forward * vec.y;
 
-        var moveDir = (right + forward).normalized;
-        moveDir.y = 0;
+        var moveDir = right + forward;
+        moveDir.y = 0f;
+
+        if (moveDir.sqrMagnitude <= 0f) { return; }
+
+        moveDir.Normalize();
 
         var speed = _modeController.ModeData.MoveSpeed * inputMag;
 
-        _rb.linearVelocity = moveDir * speed;
+        Vector3 nextPos =
+            _rb.position + moveDir * speed * Time.deltaTime;
+
+        _rb.MovePosition(nextPos);
     }
+
 
     private void Rotate()
     {
@@ -140,11 +144,14 @@ public class PlayerMovement : MonoBehaviour
         {
             while (t < _moveData.DodgeDuration)
             {
-                _rb.linearVelocity = _moveData.DodgeSpeed * dodgeDir;
+                Vector3 nextPos = _rb.position + dodgeDir * _moveData.DodgeSpeed * Time.deltaTime;
+
+                _rb.MovePosition(nextPos);
 
                 t += Time.deltaTime;
                 await UniTask.Yield(destroyCancellationToken);
             }
+
         }
         catch (OperationCanceledException)
         {
