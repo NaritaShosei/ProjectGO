b9f830ef576eb0832dfad5ed0cf3a5bd9e72af45
NaritaShosei <onigirikun7171@gmail.com>
2026-02-07 17:22:55 +0900

    feat:スキルの取得をタイプでわけれるように

--- Assets/Scripts/Data/Skill/SkillBase.cs
+++ Assets/Scripts/Data/Skill/SkillBase.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class SkillBase : ScriptableObject, ISkill
 {
@@ -7,6 +7,7 @@ public class SkillBase : ScriptableObject, ISkill
     public string Name => _name;
     public string Explanation => _explanation;
     public Sprite Icon => _icon;
+    public SkillTiming Timing => _timing;
 
     public virtual void Apply(ref AttackContext context)
     {
@@ -17,9 +18,14 @@ public class SkillBase : ScriptableObject, ISkill
         return true;
     }
 
+    public virtual void OnAcquire(IAttackStats stats)
+    {
+    }
+
     [SerializeField] private int _id;               // 検索用ID
     [SerializeField] private int _priority;         // スキル発動順優先度
     [SerializeField] private string _name;          // スキルの名前
     [SerializeField] private string _explanation;   // スキルの説明
     [SerializeField] private Sprite _icon;          // スキルのアイコン画像
+    [SerializeField] private SkillTiming _timing = SkillTiming.OnAttack; // スキルの発動タイミング
 }
