4f5b95b9b31a0a1b582b88f4d8029eaad82dd5a2
NaritaShosei <onigirikun7171@gmail.com>
2026-02-07 16:33:01 +0900

    refactor:SMBの改良

--- Assets/Scripts/Data/Player/AttackEventSMB.cs
+++ Assets/Scripts/Data/Player/AttackEventSMB.cs
@@ -9,7 +9,8 @@ public class AttackEventSMB : StateMachineBehaviour
         _attackExecuted = false;
         _comboStarted = false;
         _comboEnded = false;
-        _attackCompleted = false;
+
+        animator.TryGetComponent(out _controller);
     }
 
     public override void OnStateUpdate(
@@ -17,42 +18,42 @@ public class AttackEventSMB : StateMachineBehaviour
     {
         float t = stateInfo.normalizedTime;
 
-        var controller = animator.GetComponent<PlayerAnimationController>();
-        if (controller == null) { return; }
+        if (_controller == null) { return; }
 
         if (!_attackExecuted && t >= _attackExecuteTime)
         {
             _attackExecuted = true;
-            controller.AnimEvent_AttackExecute();
+            _controller.AnimEvent_AttackExecute();
         }
 
         if (!_comboStarted && t >= _comboWindowStartTime)
         {
             _comboStarted = true;
-            controller.AnimEvent_ComboWindowStart();
+            _controller.AnimEvent_ComboWindowStart();
         }
 
         if (!_comboEnded && t >= _comboWindowEndTime)
         {
             _comboEnded = true;
-            controller.AnimEvent_ComboWindowEnd();
-        }
-
-        if (!_attackCompleted && t >= _attackCompleteTime)
-        {
-            _attackCompleted = true;
-            controller.AnimEvent_AttackComplete();
+            _controller.AnimEvent_ComboWindowEnd();
         }
     }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _controller.AnimEvent_AttackComplete();
+    }
+
+
 
     [Header("Timings (0〜1)")]
     [SerializeField, Range(0, 1)] private float _attackExecuteTime = 0.4f;
     [SerializeField, Range(0, 1)] private float _comboWindowStartTime = 0.6f;
     [SerializeField, Range(0, 1)] private float _comboWindowEndTime = 0.8f;
-    [SerializeField, Range(0, 1)] private float _attackCompleteTime = 0.8f;
+
+    private PlayerAnimationController _controller;
 
     private bool _attackExecuted;
     private bool _comboStarted;
     private bool _comboEnded;
-    private bool _attackCompleted;
 }
