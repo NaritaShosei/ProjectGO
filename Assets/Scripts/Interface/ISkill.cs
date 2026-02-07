0250b4c26dea84c1dd986d3f82358552205b8274
NaritaShosei <onigirikun7171@gmail.com>
2026-02-07 17:21:27 +0900

    feat:Skillに発動タイミングを持たせる

--- Assets/Scripts/Interface/ISkill.cs
+++ Assets/Scripts/Interface/ISkill.cs
@@ -1,17 +1,27 @@
-﻿public interface ISkill
+using UnityEngine;
+
+public interface ISkill
 {
-    /// <summary>
-    /// 発動優先度
-    /// </summary>
+    /// <summary> 発動優先度 </summary>
     public int Priority { get; }
+    /// <summary> 発動優先度 </summary>
+    public SkillTiming Timing { get; }
 
-    /// <summary>
-    /// 発動条件
-    /// </summary>
-    public bool CanApply(AttackContext context,AttackData data);
-
-    /// <summary>
-    /// スキルを発動する,攻撃力の変化などはcontextの内部のパラメーターに行う
-    /// </summary>
+    /// <summary> 発動条件 </summary>
+    public bool CanApply(AttackContext context, AttackData data);
+    /// <summary> スキルを発動する,攻撃力の変化などはcontextの内部のパラメーターに行う </summary>
     public void Apply(ref AttackContext context);
+
+    /// <summary> スキル獲得時に一度だけ呼ばれる </summary>
+    public void OnAcquire(IAttackStats stats);
+}
+
+public enum SkillTiming
+{
+    [InspectorName("攻撃時")]
+    OnAttack,      // 攻撃時スキル
+    [InspectorName("獲得時")]
+    OnAcquire,     // 獲得時に一度だけ適用
+    [InspectorName("常時")]
+    Passive        // 常時効果
 }
