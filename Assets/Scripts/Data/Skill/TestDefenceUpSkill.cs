0bd6f6e2c9b7e426229ecd1b6e28fba64d59140e
SosukeKato <dokindokinpiano@gmail.com>
2026-02-22 08:30:10 +0900

    [feat]防御力上昇スキルを追加

---
+++ Assets/Scripts/Data/Skill/TestDefenceUpSkill.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefenceUpSkill", menuName = "GameData/Skill/DefenceUpSkill")]
+public class TestDefenceUpSkill : SkillBase
+{
+    public override bool CanAcquire(int acquireCount)
+    {
+        return acquireCount < _defenceUps.Length;
+    }
+
+    public override void OnAcquire(IPlayerStats stats, int acquireCount)
+    {
+        if (acquireCount < 1 || acquireCount > _defenceUps.Length)
+        {
+            return;
+        }
+
+        stats.AddDefensePower(_defenceUps[acquireCount - 1]);
+    }
+
+    [SerializeField] private float[] _defenceUps;
+}
