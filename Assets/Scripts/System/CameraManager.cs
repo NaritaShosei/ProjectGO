496fb9f976fa37f6d85773e8fc3ec80e1b7beb3a
NaritaShosei <onigirikun7171@gmail.com>
2026-02-27 16:30:25 +0900

    fix:カメラの不要な部分の削除

--- Assets/Scripts/System/CameraManager.cs
+++ Assets/Scripts/System/CameraManager.cs
@@ -16,8 +16,6 @@ public class CameraManager : MonoBehaviour
 
         _playerTransform = player.transform;
         _cinemachineCamera.Follow = _playerTransform;
-
-        _cinemachineTargetGroup.Targets[0].Object = _playerTransform;
     }
 
     public void LockOn(Transform target)
@@ -27,20 +25,14 @@ public class CameraManager : MonoBehaviour
             Debug.LogWarning("LockOn target is null.");
             return;
         }
-
-        _cinemachineTargetGroup.Targets[1].Object = target;
-
-        _cinemachineCamera.Follow = _cinemachineTargetGroup.transform;
     }
 
     public void Unlock()
     {
-        _cinemachineTargetGroup.Targets[1].Object = null;
         _cinemachineCamera.Follow = _playerTransform;
     }
 
     [SerializeField] private CinemachineCamera _cinemachineCamera;
-    [SerializeField] private CinemachineTargetGroup _cinemachineTargetGroup;
     private Camera _mainCamera;
     private Transform _playerTransform;
 
