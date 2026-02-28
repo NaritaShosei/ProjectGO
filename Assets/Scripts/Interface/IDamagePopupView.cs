3c70df9140434d369aa1e43e766764e0f45d7d21
NaritaShosei <onigirikun7171@gmail.com>
2026-02-28 23:47:55 +0900

    feat:ダメージ表示UIのプレゼンター

---
+++ Assets/Scripts/Interface/IDamagePopupView.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public interface IDamagePopupView 
+{
+    public void ShowDamage(DamagePopupViewModel viewModel);
+    public event Action<IDamagePopupView> OnRelease;
+}
+
+public interface IDamagePopupModel
+{
+    public event Action<DamagePopupViewModel> OnDamageHit;
+}
