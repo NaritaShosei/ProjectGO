e355af579a283308379811e23a8a1445f67da44a
NaritaShosei <onigirikun7171@gmail.com>
2026-03-13 19:04:06 +0900

    feat:モードチェンジ時のスロー

---
+++ Assets/Scripts/Data/Player/ModeChangeSMB.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+/// <summary>
+/// モードチェンジアニメーション中にスロー演出を行うSMB
+/// normalizedTimeでスロー開始・終了タイミングを指定する
+/// </summary>
+public class ModeChangeSMB : StateMachineBehaviour
+{
+    public override void OnStateEnter(
+        Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _slowApplied = false;
+
+        _hitStopManager = ServiceLocator.Get<HitStopManager>();
+    }
+
+    public override void OnStateUpdate(
+        Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float currentTime = stateInfo.normalizedTime * stateInfo.length;
+
+        if (!_slowApplied && currentTime >= _slowStartTime)
+        {
+            _slowApplied = true;
+            _hitStopManager?.TriggerDirect(_slowDuration, _targetGroup, _slowTimeScale);
+        }
+    }
+
+    [Header("スロー区間")]
+    [SerializeField] private float _slowStartTime = 0.2f;
+
+    [Header("スロー演出設定")]
+    [SerializeField] private float _slowDuration = 0.5f;
+    [SerializeField] private HitStopTargetGroup _targetGroup = HitStopTargetGroup.Player | HitStopTargetGroup.AllEnemies | HitStopTargetGroup.Effects;
+    [SerializeField] private float _slowTimeScale = 0.1f;
+
+    private HitStopManager _hitStopManager;
+    private bool _slowApplied;
+}
