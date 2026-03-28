5cf7b636bf7c821db673a8e0f0d31c517c1f5bfc
SosukeKato <dokindokinpiano@gmail.com>
2026-03-28 17:10:47 +0900

    [refactor]CodeRabbit対応

--- Assets/Scripts/Data/Skill/GroundCrush.cs
+++ Assets/Scripts/Data/Skill/GroundCrush.cs
@@ -37,11 +37,13 @@ public class GroundCrush : SkillBase
     [SerializeField] private AttackType _attackType = AttackType.LightAttack; //攻撃タイプ
     [SerializeField] private PlayerMode _isPlayerMode = PlayerMode.Warrior;   //プレイヤーが闘神モードかどうか
     [SerializeField] private float _delay = 1.0f;                             //ヒットから発動までの待機時間
-    [SerializeField] private GameObject _effectPrefab;                  //GroundSmashEffectを持つPrefab
+    [SerializeField] private GameObject _effectPrefab;                        //GroundSmashEffectを持つPrefab
 
-    private async void ActivationGroundCrush(Transform playerTransform, float attackPower)
+    private async UniTaskVoid ActivationGroundCrush(Transform playerTransform, float attackPower)
     {
         await UniTask.Delay(TimeSpan.FromSeconds(_delay));
+
+        if (playerTransform == null) return;
 
         Collider[] enemies = Physics.OverlapSphere(playerTransform.position, _attackRadius);
 
