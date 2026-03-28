e7a141d3c6733ed881e687f7268a5bbb5122c4ae
SosukeKato <dokindokinpiano@gmail.com>
2026-03-28 16:49:30 +0900

    [fix]不要だったため削除

--- Assets/Scripts/Player/AttackExecutor.cs
+++ Assets/Scripts/Player/AttackExecutor.cs
@@ -196,9 +196,6 @@ public struct AttackContext
     /// <summary>感電</summary>
     public ElectricShock ElectricShock;
 
-    /// <summary>大地粉砕Ⅱ</summary>
-    public EvolutionGroundCrush EvolutionGroundCrush;
-
     public AttackContext(PlayerMode mode, Vector3 attackPos, Transform playerTransform)
     {
         PlayerMode = mode;
@@ -213,7 +210,6 @@ public struct AttackContext
         OnAfterAttack = null;
         OnHit = null;
         ElectricShock = new();
-        EvolutionGroundCrush = new();
     }
 }
 
