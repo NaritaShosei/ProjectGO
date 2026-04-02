814ed9b5df9c808514414985b65bc80817bf8ade
野田倫太郎 <enuarle8@icloud.com>
2026-04-02 14:50:35 +0900

    feat:コード規約に沿って改善

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -4,8 +4,52 @@ using System.Collections.Generic;
 
 public class SoundManager : MonoBehaviour
 {
-    private static SoundManager _instance;
     public static SoundManager Instance => _instance;
+
+    /// <summary> BGM再生 </summary>
+    /// <param name="cueName">再生するBGMのキュー名</param>
+    /// <param name="sheetType">再生するBGMのシートの種類</param>
+    public void PlayBGM(string cueName, CueSheetType sheetType = CueSheetType.None)
+    {
+        if (sheetType != CueSheetType.None)
+            SetBGMCueSheet(_cueSheetPathRegistry.CueSheetPathDict[sheetType]);
+
+        _bgmSource.Stop();
+
+        if (_bgmSource.cueSheet != _currentBGMCueSheet)
+            _bgmSource.cueSheet = _currentBGMCueSheet;
+
+        _bgmSource.cueName = cueName;
+        _bgmSource.Play();
+    }
+
+    /// <summary> SE再生 </summary>
+    /// <param name="cueName">再生するSEのキュー名</param>
+    /// <param name="sheetType">再生するSEのシートの種類</param>
+    public void PlaySE(string cueName, CueSheetType sheetType = CueSheetType.None)
+    {
+        if (sheetType != CueSheetType.None)
+            SetSECueSheet(_cueSheetPathRegistry.CueSheetPathDict[sheetType]);
+
+        // 停止中のソースを探して再生
+        foreach (var source in _seSources)
+        {
+            if (source.status != CriAtomSource.Status.Playing)
+            {
+                if (source.cueSheet != _currentSECueSheet)
+                    source.cueSheet = _currentSECueSheet;
+
+                source.cueName = cueName;
+                source.Play();
+                return;
+            }
+        }
+
+        // 全てのソースが再生中の場合、新しいソースを作成して再生
+        CreateNewSESource().Play();
+    }
+
+    private static SoundManager _instance;
 
     private string _currentBGMCueSheet = "InGame_BGM";
     private string _currentSECueSheet = "Common_SE";
@@ -15,7 +59,7 @@ public class SoundManager : MonoBehaviour
     private CriAtomSource _bgmSource;
     private List<CriAtomSource> _seSources = new List<CriAtomSource>();
 
-    void Awake()
+    private void Awake()
     {
         if (_instance == null)
         {
@@ -39,51 +83,8 @@ public class SoundManager : MonoBehaviour
         CreateNewSESource();
     }
 
-    /// <summary> BGM再生 </summary>
-    /// <param name="cueName">再生するBGMのキュー名</param>
-    /// <param name="sheetType">再生するBGMのシートの種類</param>
-    public void PlayBGM(string cueName, CueSheetType sheetType = CueSheetType.None)
-    {
-        if(sheetType != CueSheetType.None)
-            SetBGMCueSheet(_cueSheetPathRegistry.CueSheetPathDict[sheetType]);
-
-        _bgmSource.Stop();
-
-        if(_bgmSource.cueSheet != _currentBGMCueSheet)
-            _bgmSource.cueSheet = _currentBGMCueSheet;
-
-        _bgmSource.cueName = cueName;
-        _bgmSource.Play();
-    }
-
-    /// <summary> SE再生 </summary>
-    /// <param name="cueName">再生するSEのキュー名</param>
-    /// <param name="sheetType">再生するSEのシートの種類</param>
-    public void PlaySE(string cueName, CueSheetType sheetType = CueSheetType.None)
-    {
-        if (sheetType != CueSheetType.None)
-            SetSECueSheet(_cueSheetPathRegistry.CueSheetPathDict[sheetType]);
-
-        // 停止中のソースを探して再生
-        foreach (var source in _seSources)
-        {
-            if (source.status != CriAtomSource.Status.Playing)
-            {
-                if(source.cueSheet != _currentSECueSheet)
-                    source.cueSheet = _currentSECueSheet;
-
-                source.cueName = cueName;
-                source.Play();
-                return;
-            }
-        }
-
-        // 全てのソースが再生中の場合、新しいソースを作成して再生
-        CreateNewSESource().Play();
-    }
-
     /// <summary> 新たにse用のSourceを作る処理 </summary>
-    public CriAtomSource CreateNewSESource()
+    private CriAtomSource CreateNewSESource()
     {
         var newSource = gameObject.AddComponent<CriAtomSource>();
         newSource.cueSheet = _currentBGMCueSheet;
