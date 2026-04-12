c673e35c45208d9d396fd0e67ad90c860d6dcdb4
NaritaShosei <onigirikun7171@gmail.com>
2026-04-12 19:14:49 +0900

    feat:CueSheetの文字列定数クラス

---
+++ Assets/Scripts/Sound/SoundCueNames.cs
@@ -0,0 +1,28 @@
+public static class SoundCueNames
+{
+    public static class Common
+    {
+        public const string Finish = "finish";
+        public const string ArmorBreak = "armor_break";
+        public const string ArmorHitWeak = "enemyhit_yowai_yoroi";
+        public const string BodyHitWeak = "enemyhit_yowai";
+    }
+
+    public static class Tousin
+    {
+        public const string HammerSwing = "tousin_hammerswing";
+        public const string HammerHit = "tousin_hammer_hit";
+        public const string GroundHit = "tousin_groundhit";
+        public const string ArmorHit = "enemyhit_tsuyoi_yoroi";
+        public const string BodyHit = "enemyhit_yowai";
+    }
+
+    public static class Raijin
+    {
+        public const string HammerSwing = "raijin_hammerswing";
+        public const string Attack = "raijin_attack";
+        public const string Taiden = "raijin_taiden";
+        public const string BodyHit = "enemyhit_tsuyoi";
+        public const string ArmorHit = "enemyhit_yowai_yoroi";
+    }
+}
