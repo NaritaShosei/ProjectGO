5a51dc0ddf2b1c35500439ec9d8fb66e3cec5aef
shimamura <shimakou779@gmail.com>
2026-04-18 18:56:26 +0900

    [feat]GenericObjectPoolに対応したHitEffectを作成

---
+++ Assets/Scripts/Effect/EffectPresenter.cs
@@ -0,0 +1,37 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+public class EffectPresenter
+{
+    private Effect _view;
+    private GenericObjectPool<Effect> _pool;
+
+    public EffectPresenter(Effect view,GenericObjectPool<Effect> pool)
+    {
+        _pool = pool;
+        _view = view;
+    }
+
+    public async UniTask PlayAsync(Vector3 position,Quaternion rotation, CancellationToken ct = default)
+    {
+        _view.transform.SetParent(null, false);
+        _view.transform.position = position;
+        _view.transform.rotation = rotation;
+
+        _view.Play();
+
+        // 再生終了待ち
+        await UniTask.WaitUntil(() => _view == null || !_view.IsAlive(), cancellationToken: ct);
+
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        if (_view == null) return;
+        _view.Cleanup();
+        _pool.Release(_view);
+        _view = null;
+    }
+}
