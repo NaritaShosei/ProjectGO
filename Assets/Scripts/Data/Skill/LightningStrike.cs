97f79c1af5c01b00c6e862ddd580e20bbb6129e5
NaritaShosei <onigirikun7171@gmail.com>
2026-04-19 02:39:33 +0900

    feat:Passiveを活用

--- Assets/Scripts/Data/Skill/LightningStrike.cs
+++ Assets/Scripts/Data/Skill/LightningStrike.cs
@@ -2,96 +2,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LightningStrike", menuName = "GameData/Skill/LightningStrike")]
-public class LightningStrike : SkillBase,ISkillUpdater
+public class LightningStrike : SkillBase
 {
-    public void OnUpdate(float deltaTime, PlayerMode mode, IPlayerStats stats, Vector3 playerPosition, EnemyManager enemyManager)
-    {
-        if (mode != PlayerMode.Thunder) return;
+    public int TargetCount => _targetCount;
+    public float MinInterval => _minInterval;
+    public float MaxInterval => _maxInterval;
+    public float DamageMultiplier => _damageMultiplier;
+    public float SearchRadius => _searchRadius;
+    public float AreaRadius => _areaRadius;
+    public float ElectricShockDuration => _electricShockDuration;
+    public float GrantEffectProbability => _grantEffectProbability;
+    public float UpDamagePercentage => _upDamagePercentage;
+    public GameObject[] HitEffectPrefabs => _hitEffectPrefabs;
 
-        _timer -= deltaTime;
-
-        if (_timer > 0f) return;
-
-        _timer = Random.Range(_minInterval, _maxInterval);
-
-        Trigger(stats, playerPosition, enemyManager);
-    }
+    // Timing == Passive のスキルとして CreateUpdater を実装
+    public override ISkillUpdater CreateUpdater() => new LightningStrikeUpdater(this);
 
     [SerializeField] private int _targetCount = 1;
     [SerializeField] private float _minInterval = 2f;
     [SerializeField] private float _maxInterval = 4f;
     [SerializeField] private float _damageMultiplier = 1.6f;
     [SerializeField] private float _searchRadius = 999f;
-    //エフェクト
     [SerializeField] private GameObject[] _hitEffectPrefabs;
-    //範囲
     [SerializeField] private float _areaRadius;
-    //感電時間
     [SerializeField] private float _electricShockDuration;
-
-    [System.NonSerialized] private float _timer;
-
-    private void Trigger(IPlayerStats stats, Vector3 playerPosition, EnemyManager enemyManager)
-    {
-        var enemies = enemyManager.GetEnemiesInRange(playerPosition, _searchRadius);
-
-        if (enemies.Count == 0) return;
-
-        var shuffled = new List<IEnemy>(enemies);
-        Shuffle(shuffled);
-
-        int targetCount = Mathf.Min(_targetCount, shuffled.Count);
-
-        var effects = _hitEffectPrefabs != null ? new List<GameObject>(_hitEffectPrefabs) : new List<GameObject>();
-        Shuffle(effects);
-
-        for (int i = 0; i < targetCount; i++)
-        {
-            var target = shuffled[i];
-
-            var areaTargets = enemyManager.GetEnemiesInRange(target.Position, _areaRadius);
-
-            foreach (var enemy in areaTargets)
-            {
-                float damage = stats.AttackPower * _damageMultiplier;
-
-                var damageContext = new DamageContext { AttackPower = damage, PlayerMode = PlayerMode.Thunder };
-                enemy.TakeDamage(damageContext);
-
-                enemy.ConditionController?.ApplyCondition(new ElectrifiedCondition(_electricShockDuration, enemyIsBoss: enemy.IsBoss));
-            }
-
-            GameObject effectPrefab = null;
-            if (effects.Count > 0)
-            {
-                effectPrefab = effects[i % effects.Count];
-            }
-
-            //仮のヒットエフェクト
-            if (effectPrefab != null)
-            {
-                var effect = Instantiate(effectPrefab, target.Position, Quaternion.identity);
-                Destroy(effect, 2f);
-            }
-        }
-    }
-
-    private void OnEnable()
-    {
-        _timer = Random.Range(_minInterval, _maxInterval);
-    }
-
-    /// <summary>
-    /// エフェクトシャッフル
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="list"></param>
-    private void Shuffle<T>(List<T> list)
-    {
-        for (int i = list.Count - 1; i > 0; i--)
-        {
-            int rand = Random.Range(0, i + 1);
-            (list[i], list[rand]) = (list[rand], list[i]);
-        }
-    }
+    [SerializeField, Range(0, 1)] private float _grantEffectProbability = 1f;
+    [SerializeField, Range(0, 1)] private float _upDamagePercentage = 0f;
 }
