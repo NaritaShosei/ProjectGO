a46b4ff472c4a70db7960f6d108a956e893f7e82
shimamura <shimakou779@gmail.com>
2026-04-23 21:10:28 +0900

    [feat]PlayerUIInitializerにWeaponEffectPresenterを接続

--- Assets/Scripts/System/PlayerUIInitializer.cs
+++ Assets/Scripts/System/PlayerUIInitializer.cs
@@ -9,6 +9,7 @@ public class PlayerUIInitializer : MonoBehaviour
             _playerModeController = modeController;
             _playerModePresenter = new PlayerModePresenter(_playerModeController, _playerModeView);
             _thunderEffectPresenter = new ThunderEffectPresenter(_thunderEffect, _playerModeController);
+            _weaponEffectPresenter = new WeaponEffectPresenter(_playerModeController, _weaponEffectView);
         }
         else
         {
@@ -36,12 +37,14 @@ public class PlayerUIInitializer : MonoBehaviour
     [SerializeField] private PlayerGaugeView _playerGaugeView;
     [SerializeField] private LockOnMarkerView _lockOnMarkerView;
     [SerializeField] private ThunderEffectView _thunderEffect;
+    [SerializeField] private WeaponEffectView _weaponEffectView;
 
     private IModeController _playerModeController;
     private PlayerModePresenter _playerModePresenter;
     private PlayerGaugePresenter _playerGaugePresenter;
     private LockOnMarkerPresenter _lockOnMarkerPresenter;
     private ThunderEffectPresenter _thunderEffectPresenter;
+    private WeaponEffectPresenter _weaponEffectPresenter;
 
     private void OnDestroy()
     {
@@ -49,5 +52,6 @@ public class PlayerUIInitializer : MonoBehaviour
         _playerGaugePresenter?.Dispose();
         _lockOnMarkerPresenter?.Dispose();
         _thunderEffectPresenter?.Dispose();
+        _weaponEffectPresenter?.Dispose();
     }
 }
