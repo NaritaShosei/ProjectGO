023d208854df04bcca6941e0cda1b3fc93eafce2
shimamura <shimakou779@gmail.com>
2026-04-27 23:32:31 +0900

    [refactor]Presenterに統合

--- Assets/Scripts/Effect/WeaponAuraEffect/WeaponEffectPresenter.cs
+++ Assets/Scripts/Effect/WeaponAuraEffect/WeaponEffectPresenter.cs
@@ -2,22 +2,44 @@ using System;
 
 public class WeaponEffectPresenter
 {
-    public WeaponEffectPresenter(WeaponEffectView weaponEffectView,IModeController modeController)
+    public WeaponEffectPresenter(IWeaponEffect thunderEffect,
+                                 IWeaponEffect warriorEffect,
+                                 IModeController modeController)
     {
+        _thunderEffect = thunderEffect ?? throw new ArgumentNullException(nameof(thunderEffect));
+        _warriorEffect = warriorEffect ?? throw new ArgumentNullException(nameof(warriorEffect));
         _modeController = modeController;
-        _weaponEffectView = weaponEffectView;
 
-        modeController.OnModeChanged += _weaponEffectView.Change;
+        _modeController.OnModeChanged += Change;
 
-        //初期設定しないとChangeが呼ばれないからエフェクトが出ない
-        _weaponEffectView.Change(modeController.CurrentMode);
+        // 初期反映
+        Change(_modeController.CurrentMode);
+    }
+
+    public void Change(PlayerMode mode)
+    {
+        var next = mode switch
+        {
+            PlayerMode.Thunder => _thunderEffect,
+            PlayerMode.Warrior => _warriorEffect,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        if (next == _currentEffect)
+            return;
+
+        _currentEffect?.Stop();
+        _currentEffect = next;
+        _currentEffect?.Play();
     }
 
     public void Dispose()
     {
-        _modeController.OnModeChanged -= _weaponEffectView.Change;
+        _modeController.OnModeChanged -= Change;
     }
 
+    private readonly IWeaponEffect _thunderEffect;
+    private readonly IWeaponEffect _warriorEffect;
     private readonly IModeController _modeController;
-    private readonly WeaponEffectView _weaponEffectView;
+    private IWeaponEffect _currentEffect;
 }
