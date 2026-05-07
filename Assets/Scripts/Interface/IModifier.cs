0b95bbb11ebd3775f5fa5a5110bf47d0cc075e6e
NaritaShosei <onigirikun7171@gmail.com>
2026-05-08 06:06:00 +0900

    feat:ステータス上昇インターフェース

---
+++ Assets/Scripts/Interface/IModifier.cs
@@ -0,0 +1,16 @@
+public interface IStatModifier
+{
+    StatType TargetStat { get; }
+
+    float Modify(float current);
+}
+
+public enum StatType
+{
+    Health,
+    Attack,
+    Defense,
+    CriticalRate,
+    ThunderDrain,
+    ThunderRecover
+}
