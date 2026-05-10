aa40d4211f1a4a0908bd9a0a82d5cb8631af0a3d
NaritaShosei <onigirikun7171@gmail.com>
2026-05-11 01:53:20 +0900

    feat:回避の無敵開始時間を調整可能に

--- Assets/Scripts/Player/Animation/DodgeSMB.cs
+++ Assets/Scripts/Player/Animation/DodgeSMB.cs
@@ -6,9 +6,45 @@ using UnityEngine;
 /// </summary>
 public class DodgeSMB : StateMachineBehaviour
 {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _invincibilityStarted = false;
+
+        if (animator.TryGetComponent(out PlayerAnimationController controller))
+            _playerAnimationController = controller;
+
+        var clipInfo = animator.GetCurrentAnimatorClipInfo(layerIndex);
+
+        if (clipInfo.Length > 0)
+            _frameRate = clipInfo[0].clip.frameRate;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_playerAnimationController == null) { return; }
+
+        if (_invincibilityStarted) { return; }
+
+        int currentFrame = (int)(stateInfo.normalizedTime * stateInfo.length * _frameRate);
+
+        // 無敵開始フレームに達したら無敵開始
+        if (currentFrame >= _dodgeInvincibilityStartFrame)
+        {
+            _playerAnimationController.AnimEvent_DodgeInvincibilityStart();
+            _invincibilityStarted = true;
+        }
+    }
+
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (animator.TryGetComponent(out PlayerAnimationController controller))
-            controller.AnimEvent_DodgeEnd();
+        if (_playerAnimationController != null)
+            _playerAnimationController.AnimEvent_DodgeEnd();
     }
+
+    [Header("Dodge Timings (frame)")]
+    [SerializeField] private int _dodgeInvincibilityStartFrame = 3;
+
+    private PlayerAnimationController _playerAnimationController;
+    private bool _invincibilityStarted;
+    private float _frameRate;
 }
