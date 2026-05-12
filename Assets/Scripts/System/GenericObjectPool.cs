e23e9534ff2d0c429579ebf1af3614b03bf49ed3
NaritaShosei <onigirikun7171@gmail.com>
2026-05-12 16:45:56 +0900

    feat:親が無くても生成できるように

--- Assets/Scripts/System/GenericObjectPool.cs
+++ Assets/Scripts/System/GenericObjectPool.cs
@@ -5,12 +5,11 @@ using Object = UnityEngine.Object;
 
 /// <summary>
 /// IPoolable を実装した Component を対象とするジェネリックオブジェクトプール。
-/// Get/Release 時の初期化・後始末は IPoolable.OnGet / OnRelease に委譲するため、
-/// 外部コールバック（onGet / onRelease）は廃止した。
+/// Get/Release 時の初期化・後始末は IPoolable.OnGet / OnRelease に委譲する。
 /// </summary>
 public class GenericObjectPool<T> where T : Component, IPoolable
 {
-    public GenericObjectPool(T prefab, Transform parent, int preloadCount = 0)
+    public GenericObjectPool(T prefab, Transform parent = null, int preloadCount = 0)
     {
         _prefab = prefab;
         _parent = parent;
@@ -23,12 +22,15 @@ public class GenericObjectPool<T> where T : Component, IPoolable
 
     public T Get()
     {
-        T instance = _pool.Count > 0 ? _pool.Pop() : Object.Instantiate(_prefab, _parent);
+        T instance = _pool.Count > 0
+            ? _pool.Pop()
+            : CreateInstance();
 
         _inPool.Remove(instance);
 
         instance.gameObject.SetActive(true);
         instance.OnGet();
+
         return instance;
     }
 
@@ -41,7 +43,11 @@ public class GenericObjectPool<T> where T : Component, IPoolable
             return;
 
         instance.OnRelease();
+
         instance.gameObject.SetActive(false);
+
+        if (_parent != null)
+            instance.transform.SetParent(_parent);
 
         _pool.Push(instance);
         _inPool.Add(instance);
@@ -49,16 +55,26 @@ public class GenericObjectPool<T> where T : Component, IPoolable
 
     private readonly T _prefab;
     private readonly Transform _parent;
+
     private readonly Stack<T> _pool = new();
     private readonly HashSet<T> _inPool = new();
 
     private T CreateNew()
     {
-        var instance = Object.Instantiate(_prefab, _parent);
+        var instance = CreateInstance();
+
         instance.gameObject.SetActive(false);
 
         _pool.Push(instance);
         _inPool.Add(instance);
+
         return instance;
+    }
+
+    private T CreateInstance()
+    {
+        return _parent != null
+            ? Object.Instantiate(_prefab, _parent)
+            : Object.Instantiate(_prefab);
     }
 }
