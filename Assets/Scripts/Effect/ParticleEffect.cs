a75d144b8a915197b9187c95a814b4420c9d58ba
NaritaShosei <onigirikun7171@gmail.com>
2026-05-12 12:52:24 +0900

    feat:パーティクル対応エフェクト

---
+++ Assets/Scripts/Effect/ParticleEffect.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ParticleEffect : EffectBase
+{
+    [SerializeField] private ParticleSystem _rootParticle;
+    private ParticleSystem[] _particles;
+
+    protected override void ApplyTimeScaleInternal(float scale)
+    {
+        if (_particles == null) return;
+        foreach (var ps in _particles)
+        {
+            if (ps == null) continue;
+            var main = ps.main;
+            main.simulationSpeed = scale;
+        }
+    }
+
+    protected override bool IsAliveInternal()
+    {
+        if (_particles == null) return false;
+        foreach (var ps in _particles)
+        {
+            if (ps != null && ps.IsAlive(true))
+                return true;
+        }
+        return false;
+    }
+
+    protected override void OnPlayInternal()
+    {
+        if (_rootParticle != null)
+            _rootParticle.Play();
+    }
+
+    protected override void OnStopInternal()
+    {
+        if (_rootParticle != null)
+            _rootParticle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+    }
+
+    protected override void Awake()
+    {
+        _particles = GetComponentsInChildren<ParticleSystem>(includeInactive: true);
+    }
+}
