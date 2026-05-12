3d521b747cb825c5c35c3e1ea35d8e585daa51ec
NaritaShosei <onigirikun7171@gmail.com>
2026-05-12 16:46:57 +0900

    feat:PlayableDirectorのエフェクトクラス

---
+++ Assets/Scripts/Effect/PoolingEffect/TimelineEffect.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineEffect : EffectBase
+{
+    [SerializeField] private PlayableDirector _director;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (_director == null)
+            _director = GetComponent<PlayableDirector>();
+    }
+
+    protected override void OnPlayInternal()
+    {
+        if (_director == null) return;
+
+        _director.time = 0;
+        _director.Play();
+    }
+
+    protected override void ApplyScaleInternal(Vector3 scale)
+    {
+        transform.localScale = scale;
+    }
+
+    protected override void OnStopInternal()
+    {
+        if (_director == null) return;
+
+        _director.Stop();
+        _director.time = 0;
+        _director.Evaluate();
+    }
+
+    protected override bool IsAliveInternal()
+    {
+        if (_director == null) return false;
+
+        return _director.state == PlayState.Playing;
+    }
+
+    protected override void ApplyTimeScaleInternal(float scale)
+    {
+        if (_director == null) return;
+
+        var root = _director.playableGraph.GetRootPlayable(0);
+
+        if (root.IsValid())
+        {
+            root.SetSpeed(scale);
+        }
+    }
+}
