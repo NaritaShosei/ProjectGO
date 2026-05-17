f8985a94153291aafa975e67b43f74212bc9c73c
shimamura <shimakou779@gmail.com>
2026-05-17 23:01:50 +0900

    [fix]コードラビット対応の結果よくわかんなかったので、変更を戻した

--- Assets/Scripts/Effect/HitEffect/HitEffectContext.cs
+++ Assets/Scripts/Effect/HitEffect/HitEffectContext.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public readonly struct HitEffectContext
+public  struct HitEffectContext
 {
-    public readonly Vector3 Position;
-    public readonly PlayerMode PlayerMode;
+    public Vector3 Position;
+    public PlayerMode PlayerMode;
 
-    public readonly bool IsArmorHit;
-    public readonly  bool IsArmorBreak;
+    public bool IsArmorHit;
+    public bool IsArmorBreak;
 }
