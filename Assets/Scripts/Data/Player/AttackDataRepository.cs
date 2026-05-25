f0200bfcaaab7eaef38d46bec1261d4a8b761089
NaritaShosei <onigirikun7171@gmail.com>
2026-05-25 18:57:40 +0900

    Revert "feat:作業途中。コンボを追加できるように"
    This reverts commit 30a930080d19816e248842431462443254dc1e83.

--- Assets/Scripts/Data/Player/AttackDataRepository.cs
+++ Assets/Scripts/Data/Player/AttackDataRepository.cs
@@ -1,76 +1,82 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "AttackDataRepository", menuName = "GameData/AttackDataRepository")]
 public class AttackDataRepository : ScriptableObject
 {
-    [SerializeField] private List<AttackData> _attackDatabase;
-
-    private Dictionary<string, AttackData> _attackCache;
-    private Dictionary<int, AttackData> _attackCacheIDBase;
-
-    // 実行時コンボ上書きテーブル (fromId → nextId)
-    private Dictionary<int, int> _nextComboOverrides = new();
-
+    /// <summary>
+    /// IDを基に攻撃データを検索
+    /// </summary>
     public AttackData GetAttackById(int attackId)
     {
         if (_attackCacheIDBase == null) BuildCache();
-        _attackCacheIDBase.TryGetValue(attackId, out AttackData data);
-        return data;
+
+        if (_attackCacheIDBase.TryGetValue(attackId, out AttackData data))
+        {
+            return data;
+        }
+
+        return null;
     }
 
+    /// <summary>
+    /// 与えられた攻撃の内容を基に一致する攻撃を検索
+    /// </summary>
     public AttackData GetAttackData(
-        PlayerMode mode, AttackType type, int comboIndex, ChargeLevel charge)
+        PlayerMode mode,
+        AttackType type,
+        int comboIndex,
+        ChargeLevel charge)
     {
-        if (_attackCache == null) BuildCache();
+        // 初回アクセス時に辞書登録
+        if (_attackCache == null)
+        {
+            BuildCache();
+        }
+
         string key = GetCacheKey(mode, type, comboIndex, charge);
-        _attackCache.TryGetValue(key, out AttackData data);
-        return data;
+
+        if (_attackCache.TryGetValue(key, out AttackData data))
+        {
+            return data;
+        }
+
+        return null;
     }
 
-    /// <summary>
-    /// 実行時にコンボの次の攻撃IDを上書きする
-    /// </summary>
-    public void SetNextComboOverride(int fromAttackId, int toAttackId)
-    {
-        _nextComboOverrides[fromAttackId] = toAttackId;
-    }
+    [SerializeField] private List<AttackData> _attackDatabase;
 
-    /// <summary>
-    /// attackIdの次のコンボIDを返す。オーバーライドがあれば優先する
-    /// </summary>
-    public int GetNextComboAttackId(int attackId)
-    {
-        if (_nextComboOverrides.TryGetValue(attackId, out int overrideId))
-            return overrideId;
-
-        var data = GetAttackById(attackId);
-        return data != null ? data.NextComboAttackId : -1;
-    }
-
-    /// <summary>
-    /// ランタイム状態をリセット (シーン再ロード等)
-    /// </summary>
-    public void ResetOverrides()
-    {
-        _nextComboOverrides.Clear();
-    }
+    // キャッシュ用Dictionary
+    private Dictionary<string, AttackData> _attackCache;
+    private Dictionary<int, AttackData> _attackCacheIDBase;
 
     private void BuildCache()
     {
         _attackCache = new();
         _attackCacheIDBase = new();
+
         foreach (var attack in _attackDatabase)
         {
-            if (attack == null) continue;
+            if (attack == null) { continue; }
+
             string key = GetCacheKey(
-                attack.Mode, attack.AttackType, attack.ComboIndex, attack.RequiredCharge);
+                attack.Mode,
+                attack.AttackType,
+                attack.ComboIndex,
+                attack.RequiredCharge
+            );
+
             _attackCacheIDBase[attack.AttackId] = attack;
             _attackCache[key] = attack;
         }
     }
 
     private string GetCacheKey(
-        PlayerMode mode, AttackType type, int comboIndex, ChargeLevel charge)
-        => $"{mode}_{type}_{comboIndex}_{charge}";
+        PlayerMode mode,
+        AttackType type,
+        int comboIndex,
+        ChargeLevel charge)
+    {
+        return $"{mode}_{type}_{comboIndex}_{charge}";
+    }
 }
