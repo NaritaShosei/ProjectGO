59f8c390ac119f850f6afa20c28326e3af830437
NaritaShosei <onigirikun7171@gmail.com>
2026-05-30 12:37:30 +0900

    docs:コメント追加

--- Assets/Scripts/Data/Skill/ChargeAttackSkill.cs
+++ Assets/Scripts/Data/Skill/ChargeAttackSkill.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ChargeAttackSkill", menuName = "GameData/Skill/ChargeAttackSkill")]
+
+/// <summary> 
+/// チャージ段階開放のためのスキル。
+/// 中身はないが、スキルの種類を区別するためにクラスを分けている。
+/// </summary>
 public class ChargeAttackSkill : SkillBase
 {
-
 }
