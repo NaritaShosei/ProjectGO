579b3c4e50b1be1d822f25a2a9321ebf71c07099
NaritaShosei <onigirikun7171@gmail.com>
2026-06-16 19:45:48 +0900

    refactor:ログがうるさいのでフラグで切り替え

--- Assets/Scripts/UI/Timer/CountDownTimerView.cs
+++ Assets/Scripts/UI/Timer/CountDownTimerView.cs
@@ -4,8 +4,13 @@ public class CountDownTimerView : MonoBehaviour, IPhaseTimerView
 {
     public void UpdateTimer(float current, float max)
     {
+        if (_isDebug)
+        {
+            Debug.Log($"{name} => タイマー更新: {current}/{max}");
+        }
         // タイマーのUIを更新する処理をここに実装
         // 例えば、スライダーやテキストを更新するなど
-        Debug.Log($"{name} => タイマー更新: {current}/{max}");
     }
+    [Header("チェックするとデバッグログが表示されます")]
+    [SerializeField] private bool _isDebug = false;
 }
