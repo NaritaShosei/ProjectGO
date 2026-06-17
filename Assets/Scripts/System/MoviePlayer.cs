01d6c84e4f9e7f247c9d0b7dac4614651e873a73
NaritaShosei <onigirikun7171@gmail.com>
2026-06-17 19:37:19 +0900

    feat:ムービー再生クラス

---
+++ Assets/Scripts/System/MoviePlayer.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class MoviePlayer : MonoBehaviour
+{
+    // 仮置き      
+    public event Action OnMovieStarted;
+    public event Action OnMovieFinished;
+
+    public void PlayMovie(string movieName)
+    {
+        if (_movieDictionary.TryGetValue(movieName, out var timelineAsset))
+        {
+            _playableDirector.playableAsset = timelineAsset;
+            _playableDirector.Play();
+
+            OnMovieStarted?.Invoke();
+        }
+        else
+        {
+            Debug.LogWarning($"指定されたムービー名 '{movieName}' は存在しません。");
+        }
+    }
+
+    public void StopMovie()
+    {
+        if (_playableDirector.state == PlayState.Playing)
+        {
+            _playableDirector.Stop();
+        }
+    }
+
+    // TODO:この方式だとよくない挙動が起きるかもしれない。要検討。
+    public void SkipMovie()
+    {
+        if (_playableDirector.playableAsset == null)
+            return;
+
+        if (_playableDirector.state != PlayState.Playing)
+            return;
+
+        _playableDirector.time = _playableDirector.duration;
+        _playableDirector.Evaluate();
+        _playableDirector.Stop();
+    }
+
+    [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private MovieData[] _movieData;
+
+    private readonly Dictionary<string, TimelineAsset> _movieDictionary = new Dictionary<string, TimelineAsset>();
+
+    private void Awake()
+    {
+        BuildDictionary();
+    }
+    private void OnEnable()
+    {
+        _playableDirector.stopped += OnDirectorStopped;
+    }
+
+    private void OnDisable()
+    {
+        _playableDirector.stopped -= OnDirectorStopped;
+    }
+
+    private void BuildDictionary()
+    {
+        foreach (var data in _movieData)
+        {
+            if (data.TimelineAsset == null)
+            {
+                Debug.LogWarning("TimeLineAsset が null です。MovieData の設定を確認してください。");
+                continue;
+            }
+
+            if (_movieDictionary.ContainsKey(data.Name) || string.IsNullOrEmpty(data.Name))
+            {
+                Debug.LogWarning($"MovieData の Name が重複しているか、空文字です。Name: {data.Name}");
+                continue;
+            }
+
+            _movieDictionary.Add(data.Name, data.TimelineAsset);
+        }
+    }
+
+    private void OnDirectorStopped(PlayableDirector director)
+    {
+        OnMovieFinished?.Invoke();
+    }
+}
+
+[Serializable]
+public struct MovieData
+{
+    public TimelineAsset TimelineAsset => _timelineAsset;
+    public string Name => _name;
+
+    [Header("Movie Data")]
+    [SerializeField] private TimelineAsset _timelineAsset;
+    [SerializeField] private string _name;
+}
