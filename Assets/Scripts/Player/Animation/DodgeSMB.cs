0dafad94c689542efd8a4f6350de226cae09dee1
NaritaShosei <onigirikun7171@gmail.com>
2026-06-18 20:13:07 +0900

    feat:SMBに回避終了タイミングを追加

--- Assets/Scripts/Player/Animation/DodgeSMB.cs
+++ Assets/Scripts/Player/Animation/DodgeSMB.cs
@@ -9,6 +9,7 @@ public class DodgeSMB : StateMachineBehaviour
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _invincibilityStarted = false;
+        _isDodgeEnded = false;
 
         if (animator.TryGetComponent(out PlayerAnimationController controller))
             _playerAnimationController = controller;
@@ -23,28 +24,37 @@ public class DodgeSMB : StateMachineBehaviour
     {
         if (_playerAnimationController == null) { return; }
 
-        if (_invincibilityStarted) { return; }
-
         int currentFrame = (int)(stateInfo.normalizedTime * stateInfo.length * _frameRate);
 
         // 無敵開始フレームに達したら無敵開始
-        if (currentFrame >= _dodgeInvincibilityStartFrame)
+        if (currentFrame >= _dodgeInvincibilityStartFrame && !_invincibilityStarted)
         {
             _playerAnimationController.AnimEvent_DodgeInvincibilityStart();
             _invincibilityStarted = true;
+        }
+
+        if (currentFrame >= _dodgeEndFrame && !_isDodgeEnded)
+        {
+            _playerAnimationController.AnimEvent_DodgeEnd();
+            _isDodgeEnded = true;
         }
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (_playerAnimationController != null)
+        if (_playerAnimationController != null && !_isDodgeEnded)
+        {
             _playerAnimationController.AnimEvent_DodgeEnd();
+            _isDodgeEnded = true;
+        }
     }
 
     [Header("Dodge Timings (frame)")]
-    [SerializeField] private int _dodgeInvincibilityStartFrame = 3;
+    [SerializeField, Tooltip("回避の無敵時間の開始フレーム")] private int _dodgeInvincibilityStartFrame = 3;
+    [SerializeField, Tooltip("回避終了、移動可能になるフレーム")] private int _dodgeEndFrame = 9999;
 
     private PlayerAnimationController _playerAnimationController;
     private bool _invincibilityStarted;
+    private bool _isDodgeEnded;
     private float _frameRate;
 }
