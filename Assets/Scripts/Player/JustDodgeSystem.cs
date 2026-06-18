21f9f8b5a8e3ec36a41bdfa8abf1957439d0a1b0
NaritaShosei <onigirikun7171@gmail.com>
2026-06-18 18:08:56 +0900

    feat:ジャスト回避システム

---
+++ Assets/Scripts/Player/JustDodgeSystem.cs
@@ -0,0 +1,79 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEngine;
+
+public class JustDodgeSystem : MonoBehaviour
+{
+    public event Action OnJustDodgeSuccess;
+
+    /// <summary>
+    /// ジャスト回避受付中か
+    /// </summary>
+    public bool IsJustDodgeWindow { get; private set; }
+
+    public void JustDodgeWindowStart()
+    {
+        // 前回の受付終了待機をキャンセル
+        _windowCts?.Cancel();
+        _windowCts?.Dispose();
+
+        _windowCts = new CancellationTokenSource();
+
+        IsJustDodgeWindow = true;
+
+        WindowEndAsync(_windowCts.Token).Forget();
+    }
+
+    /// <summary>
+    /// 攻撃を受けた際に呼ぶ
+    /// </summary>
+    public bool TryJustDodge()
+    {
+        if (!IsJustDodgeWindow)
+        {
+            return false;
+        }
+
+        OnJustDodgeSuccess?.Invoke();
+
+        // 1回成功したら閉じる場合
+        JustDodgeWindowEnd();
+
+        return true;
+    }
+
+    [Header("ジャスト回避判定のフレーム")]
+    [SerializeField] private int _justDodgeFrame = 15;
+
+    private CancellationTokenSource _windowCts;
+
+    private void JustDodgeWindowEnd()
+    {
+        IsJustDodgeWindow = false;
+
+        _windowCts?.Cancel();
+        _windowCts?.Dispose();
+        _windowCts = null;
+    }
+
+    private async UniTaskVoid WindowEndAsync(CancellationToken token)
+    {
+        try
+        {
+            await UniTask.DelayFrame(_justDodgeFrame, cancellationToken: token);
+
+            JustDodgeWindowEnd();
+        }
+        catch (OperationCanceledException)
+        {
+            // キャンセル時は何もしない
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _windowCts?.Cancel();
+        _windowCts?.Dispose();
+    }
+}
