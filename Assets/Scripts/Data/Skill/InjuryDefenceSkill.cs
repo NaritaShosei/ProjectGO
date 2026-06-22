7fc7af49d0930f6bba7fc369cd65c6b1ba94f6c3
SosukeKato <dokindokinpiano@gmail.com>
2026-06-22 21:22:22 +0900

    [feat]瀕死の守りスキル実装

---
+++ Assets/Scripts/Data/Skill/InjuryDefenceSkill.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InjuryDefenceSkill", menuName = "GameData/Skill/InjuryDefenceSkill")]
+
+public class InjuryDefenceSkill : SkillBase,IStatModifier
+{
+    public StatType TargetStat => StatType.Defense;
+
+    public override void OnAcquire(IPlayerStats stats)
+    {
+        _playerStats = stats;
+        stats.AddModifier(this);
+    }
+
+    public float Modify(float current)
+    {
+        if(_playerStats.CurrentHealth <= (_playerStats.MaxHealth * _isInjuryPercent))
+            return current * _defenceStatusBonusPercent;
+        else
+            return current;
+    }
+
+    [Header("防御力上昇率")]
+    [SerializeField] private float _defenceStatusBonusPercent = 1.3f;
+    [Header("防御力上昇適応体力割合")]
+    [SerializeField] private float _isInjuryPercent = 0.5f;
+
+    private IPlayerStats _playerStats;
+}
