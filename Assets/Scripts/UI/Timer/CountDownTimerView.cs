94c940aa7503bb6beb9b18c520ee239e65fb7c3c
SosukeKato <dokindokinpiano@gmail.com>
2026-06-25 00:48:39 +0900

    [feat]タイマーの表示機能実装

--- Assets/Scripts/UI/Timer/CountDownTimerView.cs
+++ Assets/Scripts/UI/Timer/CountDownTimerView.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class CountDownTimerView : MonoBehaviour, IPhaseTimerView
@@ -10,7 +11,19 @@ public class CountDownTimerView : MonoBehaviour, IPhaseTimerView
         }
         // タイマーのUIを更新する処理をここに実装
         // 例えば、スライダーやテキストを更新するなど
+
+        if(_timerText != null)
+        {
+            int min = Mathf.FloorToInt(current / 60f);
+            int sec = Mathf.FloorToInt(current % 60f);
+            int centSec = Mathf.FloorToInt(current * 100f) % 100;
+
+            _timerText.text = $"{min: 00}:{sec: 00}:{centSec: 00}";
+        }
     }
     [Header("チェックするとデバッグログが表示されます")]
     [SerializeField] private bool _isDebug = false;
+
+    [Header("テキストの参照")]
+    [SerializeField] private TextMeshProUGUI _timerText;
 }
