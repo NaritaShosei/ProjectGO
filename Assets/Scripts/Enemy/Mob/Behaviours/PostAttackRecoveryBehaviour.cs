8f3fd77994b379bd07938aa6dce3acc9c91ba1a6
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-08-06 17:26:56 +0900

    [feat]攻撃後の硬直ビヘイビア

---
+++ Assets/Scripts/Enemy/Mob/Behaviours/PostAttackRecoveryBehaviour.cs
@@ -0,0 +1,52 @@
+/// <summary>
+/// 攻撃後、一定時間その場で停止する。
+/// </summary>
+public sealed class PostAttackRecoveryBehaviour : IEnemyBehaviour
+{
+    public int Priority => (int)EnemyBehaviourPriority.Attack;
+
+    public PostAttackRecoveryBehaviour(float duration)
+    {
+        _duration = duration;
+    }
+
+    public void Init(BehaviourInitContext ctx)
+    {
+        _enemyAnimator = ctx.EnemyAnimator;
+        _state = ctx.StateContext;
+    }
+
+    public bool CanEnter()
+    {
+        return false;
+    }
+
+    public bool CanContinue()
+    {
+        return _remainingTime > 0f;
+    }
+
+    public void OnEnter()
+    {
+        _remainingTime = _duration;
+
+        _state.ChangeState(EnemyState.Idle);
+        _enemyAnimator?.SetSpeed(0f);
+    }
+
+    public void Tick(float deltaTime)
+    {
+        _remainingTime -= deltaTime;
+        _enemyAnimator?.SetSpeed(0f);
+    }
+
+    public void OnExit()
+    {
+    }
+
+    private readonly float _duration;
+
+    private float _remainingTime;
+    private IEnemyAnimator _enemyAnimator;
+    private EnemyStateContext _state;
+}
