98accbd96e6e37ca4fe7fcff64bae8d37c3aafaf
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-08-10 15:49:19 +0900

    [feat]グループでの同じ動作ができるように改変

--- Assets/Scripts/Interface/IEnemyFormationSystem.cs
+++ Assets/Scripts/Interface/IEnemyFormationSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 /// <summary>
 /// フォーメーション管理システムのインターフェース
 /// IEnemyAttackerSlotを拡張して前衛・後衛の管理機能を追加する
@@ -17,4 +19,20 @@ public interface IEnemyFormationSystem : IEnemyAttackerSlot
     /// CP同等以下の前衛と入れ替えてスロットを再配分する
     /// </summary>
     void NotifyHit(int enemyId);
+
+    /// <summary>
+    /// グループを待機状態として登録する。
+    /// 待機中のメンバーは通常の前衛選出から除外される。
+    /// </summary>
+    void RegisterWaitingGroup(EnemyGroup group);
+
+    /// <summary>
+    /// グループが前衛へ出られるか判定する。
+    /// </summary>
+    bool CanPromoteGroup(EnemyGroup group);
+
+    /// <summary>
+    /// 条件を満たしていればグループ全員を前衛へ昇格させる。
+    /// </summary>
+    bool TryPromoteGroup(EnemyGroup group);
 }
