57779645d03675234213e100801e3eddeea166b2
NaritaShosei <onigirikun7171@gmail.com>
2026-08-28 00:42:40 +0900

    feat:ModeChangePostProcessEffectPlayerとPlayerEffectInitializerをつなぐプレゼンター

---
+++ Assets/Scripts/Effect/ModeChangePostProcessEffectPresenter.cs
@@ -0,0 +1,39 @@
+using System;
+
+/// <summary>
+/// モード変更を監視し、Warrior から Thunder への切替時にポストプロセス演出を再生する。
+/// </summary>
+public sealed class ModeChangePostProcessEffectPresenter : IDisposable
+{
+    public ModeChangePostProcessEffectPresenter(
+        ModeChangePostProcessEffectPlayer effectPlayer,
+        IModeController modeController)
+    {
+        _effectPlayer = effectPlayer ?? throw new ArgumentNullException(nameof(effectPlayer));
+        _modeController = modeController ?? throw new ArgumentNullException(nameof(modeController));
+        _previousMode = modeController.CurrentMode;
+
+        _modeController.OnModeChanged += OnModeChanged;
+    }
+
+    public void Dispose()
+    {
+        _modeController.OnModeChanged -= OnModeChanged;
+        _effectPlayer.Stop();
+    }
+
+    private void OnModeChanged(PlayerMode newMode)
+    {
+        bool shouldPlay = _previousMode == PlayerMode.Warrior
+            && newMode == PlayerMode.Thunder;
+
+        _previousMode = newMode;
+
+        if (shouldPlay)
+            _effectPlayer.Play();
+    }
+
+    private readonly ModeChangePostProcessEffectPlayer _effectPlayer;
+    private readonly IModeController _modeController;
+    private PlayerMode _previousMode;
+}
