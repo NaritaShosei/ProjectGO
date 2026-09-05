ec4d30e0aefbbc766950d2e15ed83def82c60f48
野田倫太郎 <enuarle8@icloud.com>
2026-09-05 18:06:44 +0900

    Refactor：LogicをStaticに修正

--- Assets/Scripts/Enemy/Boss/System/BossEnemyCoreLogic.cs
+++ Assets/Scripts/Enemy/Boss/System/BossEnemyCoreLogic.cs
@@ -6,7 +6,7 @@ using Cysharp.Threading.Tasks;
 using System;
 using UnityEngine;
 
-namespace BossEnemy.System 
+namespace BossEnemy.Logic
 { 
 
     #region 攻撃処理
@@ -35,32 +35,35 @@ namespace BossEnemy.System
     public class Movement
     {
         /// <summary> 最終移動目標地点までの1フレーム内での移動距離を算出し移動対象を動かす処理 </summary>
-        /// <param name="moveTarget"> 移動対象 </param>
+        /// <param name="movementTarget"> 移動対象 </param>
         /// <param name="targetPos"> 最終移動目標地点 </param>
         /// <param name="moveSpeed"> 移動速度 </param>
         /// <param name="timeScale"> タイムスケール </param>
         public static void MoveTargetPosition(
-            IMoveTarget moveTarget, Vector3 targetPos, float moveSpeed, float timeScale = 1)
+            IMovement movementTarget, 
+            Vector3 targetPos, 
+            float moveSpeed, 
+            float timeScale = 1)
         {
             // speed * Time.deltaTime で「1フレームあたりの移動量」にする
             float oneFrameSpeed = moveSpeed * Time.deltaTime;
             oneFrameSpeed *= timeScale;
 
             // Y座標を動かさずに移動距離を算出
-            float savePosY = moveTarget.Position.Value.y;
-            Vector3 movePosition = Vector3.MoveTowards(moveTarget.Position.Value, targetPos, oneFrameSpeed);
+            float savePosY = movementTarget.Position.Value.y;
+            Vector3 movePosition = Vector3.MoveTowards(movementTarget.Position.Value, targetPos, oneFrameSpeed);
             movePosition.y = savePosY;
 
             // 移動速度を算出
             const float powerAdjustment = 1000; // 移動速度の力の補正をする
-            float velocityX = Mathf.Abs(moveTarget.Position.Value.x - movePosition.x) * powerAdjustment;
-            float velocityZ = Mathf.Abs(moveTarget.Position.Value.z - movePosition.z) * powerAdjustment;
+            float velocityX = Mathf.Abs(movementTarget.Position.Value.x - movePosition.x) * powerAdjustment;
+            float velocityZ = Mathf.Abs(movementTarget.Position.Value.z - movePosition.z) * powerAdjustment;
 
             // speed * Time.deltaTime で「1フレームあたりの移動量」にする
-            moveTarget.SetPosition(movePosition);
+            movementTarget.SetPosition(movePosition);
 
             // ターゲット方向への向きを算出
-            Vector3 direction = targetPos - moveTarget.Position.Value;
+            Vector3 direction = targetPos - movementTarget.Position.Value;
 
             // 高度差を無視して水平な向きにする
             direction.y = 0;
@@ -69,27 +72,30 @@ namespace BossEnemy.System
             if (direction != Vector3.zero)
             {
                 Quaternion rotation = Quaternion.LookRotation(direction);
-                moveTarget.SetRotation(rotation);
+                movementTarget.SetRotation(rotation);
             }
 
             // 移動速度設定
             Vector3 moveVelocity = new Vector3(velocityX, 0, velocityZ);
-            moveTarget.SetVelocity(moveVelocity);
+            movementTarget.SetVelocity(moveVelocity);
         }
 
         /// <summary> 最終移動目標地点まで移動目標時間ちょうどにつくための1フレーム内での移動距離を算出し移動対象を動かす処理 </summary>
-        /// <param name="moveTarget"> 移動対象 </param>
+        /// <param name="movementTarget"> 移動対象 </param>
         /// <param name="targetPos"> 最終移動目標地点 </param>
         /// <param name="time"> 移動目標時間 </param>
         /// <param name="timeScale"> タイムスケール </param>
         public static void MoveTargetPositionRightOnTime(
-            IMoveTarget moveTarget, Vector3 targetPos, float time, float timeScale = 1)
+            IMovement movementTarget, 
+            Vector3 targetPos, 
+            float time, 
+            float timeScale = 1)
         {
             // 既に目標にいる場合は処理を終了する
             if (time <= 0f) return;
 
             // 残りの距離と方向（ベクトル）を計算
-            Vector3 remainingDirection = targetPos - moveTarget.Position.Value;
+            Vector3 remainingDirection = targetPos - movementTarget.Position.Value;
 
             //「残りの距離 ÷ 残りの時間」で、今出すべき速度（秒速ベクトル）を逆算
             Vector3 requiredVelocity = remainingDirection / time;
@@ -98,10 +104,10 @@ namespace BossEnemy.System
             Vector3 frameMovement = requiredVelocity * Time.deltaTime * timeScale;
 
             // 現在の座標に移動量を足した「到達すべき座標」を返す
-            moveTarget.SetPosition(moveTarget.Position.Value + frameMovement);
+            movementTarget.SetPosition(movementTarget.Position.Value + frameMovement);
 
             // ターゲット方向への向きを算出
-            Vector3 direction = targetPos - moveTarget.Position.Value;
+            Vector3 direction = targetPos - movementTarget.Position.Value;
 
             // 高度差を無視して水平な向きにする
             direction.y = 0;
@@ -110,19 +116,19 @@ namespace BossEnemy.System
             if (direction != Vector3.zero)
             {
                 Quaternion rotation = Quaternion.LookRotation(direction);
-                moveTarget.SetRotation(rotation);
+                movementTarget.SetRotation(rotation);
             }
         }
 
-        /// <summary> 対象の方角を一定の速度で </summary>
-        /// <param name="moveTarget"> 移動対象 </param>
+        /// <summary> 動きを作る対象を対象の方角へ一定の速度で振り向かせる </summary>
+        /// <param name="movementTarget"> 移動対象 </param>
         /// <param name="targetPos"> 振り向き対象 </param>
         /// <param name="lookSpeed"> 振り向き速度 </param>
         /// <param name="finishAngleThreshold"> 振り向き対象の方角を向いていると判定できる角度の最低誤差 </param>
         /// <param name="isLookingAtTarget"> 振り向き対象の方角を向いているフラグ </param>
         /// <param name="timeScale"> タイムスケール </param>
         public static void LookAtTarget(
-            IMoveTarget moveTarget, 
+            IMovement movementTarget, 
             Vector3 targetPos, 
             float lookSpeed, 
             float finishAngleThreshold, 
@@ -130,11 +136,11 @@ namespace BossEnemy.System
             float timeScale = 1)
         {
             // ターゲットへの方向ベクトルを計算（自身の位置からターゲットの位置を引く）
-            Vector3 direction = targetPos - moveTarget.Position.Value;
+            Vector3 direction = targetPos - movementTarget.Position.Value;
 
             // 移動速度設定
             Vector3 moveVelocity = new Vector3(Mathf.Abs(direction.x), 0, Mathf.Abs(direction.z));
-            moveTarget.SetVelocity(moveVelocity);
+            movementTarget.SetVelocity(moveVelocity);
 
             // 上下の傾き（Y成分）を無視して、水平方向の回転のみにする
             direction.y = 0f;
@@ -146,8 +152,8 @@ namespace BossEnemy.System
                 Quaternion targetRotation = Quaternion.LookRotation(direction);
 
                 // 現在の回転から目標の回転へ、Time.deltaTimeをかけてゆっくり補間
-                moveTarget.SetRotation(Quaternion.Slerp(moveTarget.Rotation.Value, targetRotation, lookSpeed * Time.deltaTime * timeScale));
-                float angleDiff = Quaternion.Angle(moveTarget.Rotation.Value, targetRotation);
+                movementTarget.SetRotation(Quaternion.Slerp(movementTarget.Rotation.Value, targetRotation, lookSpeed * Time.deltaTime * timeScale));
+                float angleDiff = Quaternion.Angle(movementTarget.Rotation.Value, targetRotation);
 
                 // 角度の差がしきい値以下になったかチェック
                 if (angleDiff <= finishAngleThreshold)
