68092cbb9c447caaf57bb0e331f328b1218ca309
野田倫太郎 <enuarle8@icloud.com>
2026-09-05 18:00:37 +0900

    Fix：BossのSpawnを非同期で行う

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,8 +1,11 @@
+using BossEnemy.Interface;
+using BossEnemy.UI;
+using Cysharp.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using UnityEngine;
-using BossEnemy.Interface;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -32,12 +35,12 @@ public class EnemyManager : MonoBehaviour
         _playerInformationService = new PlayerInformationService(_player, this);
 
         _enemyServices = new EnemyServices(
-       _spatialHashGrid,
-       _separationService,
-       _wallAvoidanceService,
-       _formationSystem,
-       _playerInformationService
-   );
+        _spatialHashGrid,
+        _separationService,
+        _wallAvoidanceService,
+        _formationSystem,
+        _playerInformationService
+        );
 
         // 中ボス生成時にプレイヤーレベルを参照するためEXPManagerを取得
         if (!ServiceLocator.TryGet(out _expManager))
@@ -94,8 +97,6 @@ public class EnemyManager : MonoBehaviour
             }
 
             OnEnemySpawned?.Invoke(enemy);
-
-            enemy.Init();
 
             if (enemy is Enemy enemyComponent)
             {
@@ -290,7 +291,7 @@ public class EnemyManager : MonoBehaviour
     }
 
     /// <summary> ボスを生成 </summary>
-    public void SpawnBoss(string poolKey, Vector3 pos)
+    public async UniTask SpawnBoss(string poolKey, Vector3 pos)
     {
         if (_player == null)
         {
@@ -298,7 +299,8 @@ public class EnemyManager : MonoBehaviour
             return;
         }
 
-        IBossEnemyCharacterView enemy =　_bossEnemySpawner.Spawn(pos, out IBossHPView bossEnemyUIView);
+        BossEnemyHPView bossEnemyUIView = null;
+        IBossEnemyCharacterView enemy = await _bossEnemySpawner.Spawn(pos, bossEnemyUIView);
         if (enemy == null) return;
 
         // Enemy死亡時と被弾時のイベント登録
@@ -310,10 +312,10 @@ public class EnemyManager : MonoBehaviour
 
         OnEnemySpawned?.Invoke(enemy);
 
-        enemy.Init();
-
         _spatialHashGrid.Register(enemy, pos);
         _enemies.Add(enemy);
+
+        enemy.StartAction();
     }
 
     /// <summary> スポーン中のモブ敵をプールに返して非有効化する </summary>
