33d541e145682ab1615f333d27af10a4e8d34bee
野田倫太郎 <enuarle8@icloud.com>
2026-09-13 23:32:20 +0900

    Fix:姿勢切り替え処理の重複対策

--- Assets/Scripts/Enemy/Boss/Character/BossCharacterEntity.cs
+++ Assets/Scripts/Enemy/Boss/Character/BossCharacterEntity.cs
@@ -312,6 +312,11 @@ namespace BossEnemy.Character
         /// <param name="postureType"> 変更後の姿勢 </param>
         public void SetCharacterPosture(PostureType postureType)
         {
+            // 既に現在の姿勢切り替え中だった場合の対策一度CharacterAction.Idleにすることで
+            // もう一度現在の行動がPostureChangingに切り替わった際のイベントを発火させる
+            if (_currentCharacterAction.Value == CharacterAction.PostureChanging)
+                SetCurrentAction(CharacterAction.Idle);
+
             if (_currentPostureType == postureType) return;
 
             _currentPostureType = postureType;
