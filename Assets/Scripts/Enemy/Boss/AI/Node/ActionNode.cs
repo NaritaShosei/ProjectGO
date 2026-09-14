d3d4cce680a7425c93f64fcab0d3a760d25ee0b3
野田倫太郎 <enuarle8@icloud.com>
2026-09-15 08:28:21 +0900

    Fix：鎧の修復

--- Assets/Scripts/Enemy/Boss/AI/Node/ActionNode.cs
+++ Assets/Scripts/Enemy/Boss/AI/Node/ActionNode.cs
@@ -29,6 +29,31 @@ namespace BossEnemy.AI.BehaviourTree
     }
 
     [Serializable]
+    public class ArmorRepairAction : ActionNode
+    {
+        public void SetRepairArmor(ArmorAttachmentType repairArmor)
+        {
+            _repairArmor = repairArmor;
+        }
+
+        public override void OnEnter()
+        {
+            _bossCharacterEntity.RepairArmor(_repairArmor);
+        }
+
+        public override void OnUpdate()
+        {
+            if(_bossCharacterEntity.RepairArmorAttachmentType.Value
+                == ArmorAttachmentType.None)
+            {
+                HandleRunningEnd();
+            }
+        }
+
+        [SerializeField] private ArmorAttachmentType _repairArmor;
+    }
+
+    [Serializable]
     public class WaitForTimeAction : ActionNode
     {
         public void SetWaitTime(float waitTime)
