dc4bf187c00577df88b790b3b60bd6df8f11d640
Oriku0308 <O.riku8863@gmail.com>
2026-09-17 11:47:45 +0900

    [fix]被弾後の無敵時間をめっちゃ長くした、後ほど被弾ステートから抜けるタイミングぐらいの長さに調整する

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -211,10 +211,11 @@ public class Player : MonoBehaviour, IPlayer, ISpeedChange
                 SuppressDamageVoice = !shouldReact
             });
 
-        // ダメージを受けたら、一定時間ダメージ無敵にする。これにより、連続でダメージを受けるのを防ぐ。
+        // ダメージを受けたら、被弾硬直中はダメージ無敵にする。これにより、連続でダメージを受けるのを防ぐ。
+        // 通常は被弾硬直アニメーション終了時(HandleDamagedEnd)に解除される。
         _playerStateManager.AddInvincible(InvincibleType.Damaged);
 
-        // ダメージ無敵を解除するタイミングは、プレイヤーデータで設定された時間経過後。これにより、ダメージを受けた後の無敵時間を柔軟に設定できる。
+        // アニメーションイベントが発火しなかった場合に無敵が残り続けないための安全網。
         HandleDamageInvincibilityEnd(_playerData.GetDamageInvincibleDuration(reactionType)).Forget();
 
         // ダメージリアクションを発生させていいと判断された場合、状態をダメージ状態に遷移させる。      
