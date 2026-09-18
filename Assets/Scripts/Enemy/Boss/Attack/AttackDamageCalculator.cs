7032454a941d05c3efe3f73ea64833e661bb183b
野田倫太郎 <enuarle8@icloud.com>
2026-09-19 01:28:19 +0900

    Fix：計算に使用する変数をfloatに変更

--- Assets/Scripts/Enemy/Boss/Attack/AttackDamageCalculator.cs
+++ Assets/Scripts/Enemy/Boss/Attack/AttackDamageCalculator.cs
@@ -5,15 +5,15 @@ namespace BossEnemy.Attack
     /// <summary> ボスの攻撃計算機 </summary>
     public class AttackDamageCalculator
     {
-        private const int AttackPowerDivisor = 100;
+        private const float AttackPowerDivisor = 100;
 
         /// <summary> ボスの攻撃のダメージ計算処理 </summary>
         /// <param name="attackPower"> 攻撃力 </param>
         /// <param name="damage"> 技のダメージ </param>
         /// <returns>  </returns>
-        public static int AttackDamageCalculate(int attackPower, int damage)
+        public static float AttackDamageCalculate(float attackPower, float damage)
         {
-            var totalDamage = damage * (attackPower / AttackPowerDivisor);
+            float totalDamage = damage * (attackPower / AttackPowerDivisor);
 
             return totalDamage;
         }
